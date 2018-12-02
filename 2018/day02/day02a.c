8a3108c1cf0174b27340f753841897ae78f90800
Sijmen J. Mulder <ik@sjmulder.nl>
2018-12-02 10:23:14 +0100

    2018 day 2

---
+++ 2018/day02/day02a.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <err.h>
+
+int ccount(char *, char);
+
+int
+main()
+{
+	char line[32]; 
+	char c;
+	int have2, n2 = 0;
+	int have3, n3 = 0;
+	int cm;
+
+	while (fgets(line, sizeof(line), stdin)) {
+		have2 = 0;
+		have3 = 0;
+		for (c = 'a'; c <= 'z'; c++) {
+			cm = ccount(line, c);
+			if (cm >= 3)
+				have3 = 1;
+			else if (cm == 2)
+				have2 = 1;
+		}
+		if (have2)
+			n2++;
+		if (have3)
+			n3++;
+	}
+
+	if (ferror(stdin))
+		err(1, "fgets");
+	
+	printf("%d\n", n2 * n3);
+}
+
+int
+ccount(char *s, char c)
+{
+	int count = 0;
+
+	while (*s)
+		if (*(s++) == c)
+			count++;
+
+	return count;
+}
---
+++ 2018/day02/day02b.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <err.h>
+
+static int sndiff(char *, char *);
+static void pcommon(char *, char *);
+
+int
+main()
+{
+	char lines[256][32];
+	size_t n = 0;
+	size_t i, j;
+
+	while (n < sizeof(lines) / sizeof(*lines)) {
+		if (fgets(lines[n], sizeof(*lines), stdin))
+			n++;
+		else if (ferror(stdin))
+			err(1, "<stdin>");
+		else
+			break;
+	}
+
+	for (i = 0; i < n; i++) {
+		for (j = i+1; j < n; j++) {
+			if (sndiff(lines[i], lines[j]) == 1) {
+				pcommon(lines[i], lines[j]);
+				return 0;
+			}
+		}
+	}
+
+	puts("no solution");
+	return 0;
+}
+
+static int
+sndiff(char *s1, char *s2)
+{
+	int ndiff = 0;
+
+	while (*s1 && *s2)
+		if (*(s1++) != *(s2++))
+			ndiff++;
+	
+	return ndiff + strlen(s1) + strlen(s2);
+}
+
+static void
+pcommon(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+		if (*(s1++) == *(s2++))
+			putchar(s1[-1]);
+}
