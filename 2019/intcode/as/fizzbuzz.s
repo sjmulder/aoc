94620c55bb0df485e9c607a681cc13ff64b1895a
Sijmen J. Mulder <ik@sjmulder.nl>
2019-12-06 18:03:24 +0100

    Fix fizzbuzz.s

--- 2019/intcode/as/fizzbuzz.s
+++ 2019/intcode/as/fizzbuzz.s
@@ -1,12 +1,12 @@
 loop:
 	add 1, [n], [n]
-	eq n:0, 101, [is_done]; jt is_done:0, [done]
+	eq n:0, 101, [is_done]; jt is_done:0, done
 
 	eq next_fibu:15, [n], [is_fibu]; jt is_fibu:0, fibu
 	eq next_fizz: 3, [n], [is_fizz]; jt is_fizz:0, fizz
 	eq next_buzz: 5, [n], [is_buzz]; jt is_buzz:0, buzz
 	out [n]
-	jt 1, [loop]
+	jt 1, loop
 
 fibu:
 	out 88888
