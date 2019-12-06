cfdbc7ee0f133b271db5d11987972a57bb5e4e40
Sijmen J. Mulder <ik@sjmulder.nl>
2019-12-06 02:24:54 +0100

    Cooler fizzbuzz implementation

--- 2019/intcode/as/fizzbuzz.s
+++ 2019/intcode/as/fizzbuzz.s
@@ -1,32 +1,29 @@
-loop:	add	[n],	1,	[n]
-	eq	[n],	101,	[acc]
-	jt	[acc],	end
-	eq	[n],	[f_at],	[acc]
-	jt	[acc],	fizz
-	eq	[n],	[b_at],	[acc]
-	jt	[acc],	buzz
-	out	[n]
-	jt	1,	loop
+loop:
+	add [n], 1, [n]
+	eq n:0, 101, [is_done]; jt is_done:0, [done]
 
-fizz:	eq	[n],	[b_at],	[acc]
-	jt	[acc],	fibu
-	out	33333
-	add	[f_at],	3,	[f_at]
-	jt	1,	loop
+	eq [n], next_fibu:15, [is_fibu]; jt is_fibu:0, fibu
+	eq [n], next_fizz: 3, [is_fizz]; jt is_fizz:0, fizz
+	eq [n], next_buzz: 5, [is_buzz]; jt is_buzz:0, buzz
+	out [n]
+	jt 1, [loop]
 
-buzz:	out	55555
-	add	[b_at],	5,	[b_at]
-	jt	1,	loop
+fibu:
+	out 88888
+	add [next_fibu], 15, [next_fibu]
+	add [next_fizz],  3, [next_fizz]
+	add [next_buzz],  5, [next_buzz]
+	jt 1, loop
 
-fibu:	out	88888
-	add	[f_at],	3,	[f_at]
-	add	[b_at],	5,	[b_at]
-	jt	1,	loop
+fizz:
+	out 33333
+	add [next_fizz], 3, [next_fizz]
+	jt 1, loop
 
-end:	hlt
+buzz:
+	out 55555
+	add [next_buzz], 5, [next_buzz]
+	jt 1, loop
 
-acc:	0
-n:	0
-f_at:	3
-b_at:	5
-fb_at:	15
+done:
+	hlt
