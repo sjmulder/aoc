4f1085492bded6ada4d4c41db18df0a269a3e132
Sijmen J. Mulder <ik@sjmulder.nl>
2019-12-06 02:34:43 +0100

    Optimise for size

--- 2019/intcode/as/fizzbuzz.s
+++ 2019/intcode/as/fizzbuzz.s
@@ -1,28 +1,28 @@
 loop:
-	add [n], 1, [n]
+	add 1, [n], [n]
 	eq n:0, 101, [is_done]; jt is_done:0, [done]
 
-	eq [n], next_fibu:15, [is_fibu]; jt is_fibu:0, fibu
-	eq [n], next_fizz: 3, [is_fizz]; jt is_fizz:0, fizz
-	eq [n], next_buzz: 5, [is_buzz]; jt is_buzz:0, buzz
+	eq next_fibu:15, [n], [is_fibu]; jt is_fibu:0, fibu
+	eq next_fizz: 3, [n], [is_fizz]; jt is_fizz:0, fizz
+	eq next_buzz: 5, [n], [is_buzz]; jt is_buzz:0, buzz
 	out [n]
 	jt 1, [loop]
 
 fibu:
 	out 88888
-	add [next_fibu], 15, [next_fibu]
-	add [next_fizz],  3, [next_fizz]
-	add [next_buzz],  5, [next_buzz]
+	add 15, [next_fibu], [next_fibu]
+	add  3, [next_fizz], [next_fizz]
+	add  5, [next_buzz], [next_buzz]
 	jt 1, loop
 
 fizz:
 	out 33333
-	add [next_fizz], 3, [next_fizz]
+	add 3, [next_fizz], [next_fizz]
 	jt 1, loop
 
 buzz:
 	out 55555
-	add [next_buzz], 5, [next_buzz]
+	add 5, [next_buzz], [next_buzz]
 	jt 1, loop
 
 done:
