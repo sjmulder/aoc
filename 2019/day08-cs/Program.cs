395065bfe8c4b0a4292265e56e6c51e1eab2b003
Sijmen J. Mulder <ik@sjmulder.nl>
2019-12-09 00:12:47 +0100

    2019 day 8 with C#/Linq

---
+++ 2019/day08-cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Aoc.Util;
+
+class Program
+{
+    const int W = 25;
+    const int H = 6;
+
+    static async Task Main(string[] args)
+    {
+        var layers = (await File
+            .ReadAllTextAsync("input"))
+            .TrimEnd('\n')
+            .Slice(W * H)
+            .Select(cs => new String(cs))
+            .ToArray();
+
+        var minLayer = layers
+            .MinBy(l => l.Count(c => c == '0'));
+        int part1 =
+            minLayer.Count(c => c == '1') *
+            minLayer.Count(c => c == '2');
+
+        Console.WriteLine($"Part 1: {part1}");
+
+        var image = layers
+            .Aggregate(
+                Enumerable.Repeat('2', W * H),
+                (acc, l) => acc
+                    .Zip(l)
+                    .Select(p => p.First == '2' ? p.Second : p.First))
+            .Select(c => c == '1' ? '#' : ' ')
+            .Slice(W)
+            .Select(cs => new String(cs))
+            .ToArray();
+
+        Console.WriteLine($"Part 2:");
+
+        foreach (var line in image)
+            Console.WriteLine(line);
+    }
+}
---
+++ 2019/util-cs/LinqExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aoc.Util
+{
+    public static class Linq
+    {
+        public static IEnumerable<T[]> Slice<T>(
+            this IEnumerable<T> xs,
+            int count)
+        {
+            var enumerator = xs.GetEnumerator();
+            var list = new List<T>(count);
+
+            while (enumerator.MoveNext())
+            {
+                list.Add(enumerator.Current);
+                if (list.Count >= count)
+                {
+                    yield return list.ToArray();
+                    list = new List<T>(count);
+                }
+            }
+
+            if (list.Count > 0)
+                yield return list.ToArray();
+        }
+
+        public static T MinBy<T, TKey>(
+                this IEnumerable<T> xs,
+                Func<T, TKey> selector)
+            where TKey : IComparable<TKey>
+        {
+            var minElem = default(T);
+            var minVal = default(TKey);
+            bool haveMin = false;
+
+            foreach (var x in xs)
+            {
+                var val = selector(x);
+                if (!haveMin || val.CompareTo(minVal) < 0)
+                {
+                    minVal = val;
+                    minElem = x;
+                    haveMin = true;
+                }
+            }
+
+            return minElem;
+        }
+    }
+}
