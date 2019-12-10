57283cb782a60dcfcff7ad2df635ed03eebce87f
Sijmen J. Mulder <ik@sjmulder.nl>
2019-12-10 07:56:07 +0100

    2019 day 8 in point-free F#

---
+++ 2019/day08-fs/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+
+let dup f a = f a a
+let iff t f b = if b then t else f
+let fromChars cs = new string(Seq.toArray cs)
+
+let readInput = String.filter (not << Char.IsWhiteSpace) << File.ReadAllText
+let toLayers = (>>) Seq.chunkBySize << (>>) << Seq.chunkBySize
+let countInLayer = (<<) Seq.sum << Seq.map << (<<) Seq.length << Seq.where << (=)
+let fullestLayer = (<<) Seq.minBy countInLayer '0'
+let flattenPixels = (<<) Seq.head << Seq.filter <| (<>) '2'
+let flattenLayers = (<<) Seq.map Seq.map flattenPixels << Seq.map Seq.transpose << Seq.transpose
+let toDisplayChar = iff '#' ' ' << (=) '1'
+let toDisplay = String.concat "\r\n" << Seq.map (fromChars << Seq.map toDisplayChar)
+let part1Score = (<<) (>>) countInLayer '2' << (*) << countInLayer '1' |> dup
+let part1 = part1Score << fullestLayer
+let part2 = toDisplay << flattenLayers
+
+[<EntryPoint>]
+let main argv =   
+    let layers = toLayers 25 6 <| readInput "input"
+    printfn "Part 1:%d" <| part1 layers
+    printfn "Part 2:\n%s" <| part2 layers
+    0
