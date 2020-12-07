ff086b4c442428ecf89071aa38c00f601f84e7ad
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-07 12:55:32 +0100

    2015 day 13

---
+++ 2015/day13/solve-noname.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <err.h>
+
+#define CAP 64
+
+static int prefs[CAP][CAP];
+static int count;
+
+static void
+parse(void)
+{
+	char first[16], who[16], whom[16], what[5];
+	int i=1,j=0, n, pts;
+
+	count = CAP;
+
+	for (i=0; i<count; i++)
+	for (j=0; j<count; j++) {
+		if (i == j) continue;
+
+		n = scanf(" %15s would %4s %d happiness units by "
+		    "sitting next to %15s", who, what, &pts, whom);
+
+		if (n != 4)                errx(1, "not enough data");
+		if (!strcmp(what, "lose")) pts *= -1;
+
+		if (i) ;
+		else if (j==1)               memcpy(first, who, 16);
+		else if (strcmp(first, who)) { count = j; i=1; j=0; }
+
+		prefs[i][j] = pts;
+	}
+}
+
+static int
+solve(void)
+{
+	static int seats[CAP];
+	int i,j, best=INT_MIN, score;
+
+	while (1) {
+skip:		for (i = count-1; seats[i] == count-1; )
+			if (!i--)
+				return best;
+		seats[i]++;
+		for (i = i+1; i<count; i++)
+			seats[i] = 0;
+
+		for (i=0; i<count; i++)
+		for (j=0; j<i; j++)
+			if (seats[i] == seats[j])
+				goto skip;
+
+		score = 0;
+		for (i=0; i < count; i++) {
+			score += prefs[seats[i]][seats[(i+1) % count]];
+			score += prefs[seats[(i+1) % count]][seats[i]];
+		}
+
+		if (score > best)
+			best = score;
+	}
+}
+
+int
+main()
+{
+	parse();
+
+	printf("%d\n", solve()); count++;
+	printf("%d\n", solve());
+}
---
+++ 2015/day13/solve.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <err.h>
+
+#define LEN(x) ((int)(sizeof(x)/sizeof(*(x))))
+
+#define VERBOSE 0
+
+static char names[64][16];
+static int prefs[LEN(names)][LEN(names)];
+static int nnam;
+
+static int
+nameidx(char *name)
+{
+	int i;
+
+	for (i=0; i<nnam; i++)
+		if (!strcmp(name, names[i]))
+			return i;
+
+	if (++nnam >= LEN(names)) /* leave 1 for part 2 */
+		errx(1, "name overflow");
+
+	snprintf(names[i], LEN(names[i]), "%s", name);
+	return i;
+}
+
+static void
+parse(void)
+{
+	char who[16], whom[16], what[5];
+	int n, pts;
+
+	while (1) {
+		n = scanf(" %15s would %4s %d happiness units by "
+		    "sitting next to %15s", who, what, &pts, whom);
+		if (n != 4)
+			break;
+		if (!strcmp(what, "lose"))
+			pts *= -1;
+
+		whom[strlen(whom)-1] = '\0';
+		prefs[nameidx(who)][nameidx(whom)] = pts;
+	}
+}
+
+static void
+dump(void)
+{
+	int i,j;
+
+	for (i=0; i<nnam; i++)
+	for (j=0; j<nnam; j++)
+		printf("%s %s %d\n", names[i], names[j], prefs[i][j]);
+}
+
+static int
+solve(void)
+{
+	static int seats[LEN(names)];
+	int i,j, best=INT_MIN, score;
+
+	while (1) {
+		for (i = nnam-1; i>=0 && seats[i] == nnam-1; i--)
+			;
+		if (i == -1)
+			break;
+		seats[i]++;
+		for (i = i+1; i<nnam; i++)
+			seats[i] = 0;
+
+		if (VERBOSE)
+			for (i=0; i<nnam; i++)
+				printf("%s\t", names[seats[i]]);
+
+		for (i=0; i<nnam; i++)
+		for (j=0; j<i; j++)
+			if (seats[i] == seats[j]) {
+				if (VERBOSE) puts("");
+				goto end;
+			}
+
+		score = 0;
+		for (i=0; i < nnam; i++) {
+			score += prefs[seats[i]][seats[(i+1) % nnam]];
+			score += prefs[seats[(i+1) % nnam]][seats[i]];
+		}
+
+		if (VERBOSE)
+			printf("%d\n", score);
+		if (score > best)
+			best = score;
+	end: ;
+	}
+
+	return best;
+}
+
+int
+main()
+{
+	parse();
+	if (VERBOSE) dump();
+
+	printf("%d\n", solve()); nnam++;
+	printf("%d\n", solve());
+}
