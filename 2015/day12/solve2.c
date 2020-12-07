87e0c8025016c3f01a50b7e8bcaaf2ae83bd4f68
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-07 12:54:10 +0100

    2015 day 12 part 2

---
+++ 2015/day12/solve2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+
+static int
+recur(char end)
+{
+	int c=0,n=0, sign=1, acc=0;
+	char *match = "\"red\"";
+
+	while (c != end && c != EOF) {
+		c = getchar();
+
+		if (end != '}' || c != *match)
+			match = "\"red\"";
+		else if (!*(++match)) {
+			while ((c = getchar()) != EOF)
+				if (c == end) break;
+				else if (c == '{') recur('}');
+				else if (c == '[') recur(']');
+			return 0;
+		}
+
+		if (isdigit(c))    n = n*10 + (c-'0');
+		else if (c == '[') acc += recur(']');
+		else if (c == '{') acc += recur('}');
+		else {
+			acc += sign * n;
+			sign = c=='-' ? -1 : 1;
+			n = 0;
+		}
+	}
+
+	return acc;
+}
+
+int main() { printf("%d\n", recur(-1)); }
