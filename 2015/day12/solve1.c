13f8a964a9077f95512311b54ee5f2ded750da3a
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-07 02:05:25 +0100

    2015 day 12 part 1

---
+++ 2015/day12/solve1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int
+main()
+{
+	int c=0, n=0, sign=1, acc=0;
+
+	while (c != EOF)
+		if (isdigit((c = getchar())))
+			n = n*10 + (c-'0');
+		else {
+			acc += sign * n;
+			sign = c=='-' ? -1 : 1;
+			n = 0;
+		}
+
+	printf("%d\n", acc);
+}
