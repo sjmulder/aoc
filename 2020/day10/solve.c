50e8f2e646c90b244c34f1180f5638691d5fc33f
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-10 10:33:40 +0100

    2020 day 10

---
+++ 2020/day10/solve.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+#define CAP 128
+
+static int
+cmp(const void *a, const void *b)
+{
+	return *(int *)a - *(int *)b;
+}
+
+int
+main()
+{
+	static int nums[CAP];
+	static long npaths[CAP];
+	int i,j, len=1, n1=0,n3=1;
+
+	while (scanf(" %d", &nums[len]) == 1)
+		if (++len >= CAP)
+			errx(1, "nums overflow");
+
+	qsort(nums, len, sizeof(nums[0]), cmp);
+
+	for (i=1; i<len; i++)
+		switch (nums[i]-nums[i-1]) {
+			case 1: n1++; break;
+			case 3: n3++; break;
+		}
+
+	npaths[len-1] = 1;
+	for (i=len-2; i>=0; i--)
+		for (j=i+1; j<len && nums[j]-nums[i] <4; j++)
+			npaths[i] += npaths[j];
+	
+	printf("%d %ld\n", n1*n3, npaths[0]);
+}
