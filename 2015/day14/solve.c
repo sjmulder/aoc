f9722187e86e100efae0c6fd37b098d5cc1400e7
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-10 22:48:57 +0100

    2015 day 14

---
+++ 2015/day14/solve.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+#define CAP 16
+
+const char fmt[] =
+"%*s can fly %d km/s for %d seconds, but then must rest for %d "
+"seconds.";
+
+int
+main(int argc, char **argv)
+{
+	static struct deer { int kms, fly, rest, dist, pts; } d[CAP];
+	int t,i, tend=2503, z=0, maxdist=0, maxpts=0;
+
+	if (argc > 1)
+		tend = atoi(argv[1]);
+
+	while (scanf(fmt, &d[z].kms, &d[z].fly, &d[z].rest) == 3)
+		if (++z >= CAP)
+			errx(1, "out of bounds");
+
+	for (t=0; t<tend; t++) {
+		for (i=0; i<z; i++) {
+			if (t % (d[i].fly + d[i].rest) < d[i].fly)
+				d[i].dist += d[i].kms;
+			if (maxdist < d[i].dist)
+				maxdist = d[i].dist;
+		}
+		for (i=0; i<z; i++)
+			d[i].pts += d[i].dist == maxdist;
+	}
+
+	for (i=0; i<z; i++) {
+		if (maxdist < d[i].dist)
+			maxdist = d[i].dist;
+		if (maxpts < d[i].pts)
+			maxpts = d[i].pts;
+	}
+
+	printf("%d %d\n", maxdist, maxpts);
+}
---
+++ 2015/day14/solve1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char fmt[] =
+"%*s can fly %d km/s for %d seconds, but then must rest for %d "
+"seconds.";
+
+int
+main(int argc, char **argv)
+{
+	int t,d, tend=2503, best=0, kms, fly, rest;
+
+	if (argc > 1)
+		tend = atoi(argv[1]);
+
+	while (scanf(fmt, &kms, &fly, &rest) == 3) {
+		d=0; t=tend;
+
+		while (t>fly)
+			{ d += kms*fly; t -= fly+rest; }
+		if (t>0)
+			d += t * kms;
+		if (d>best)
+			best = d;
+	}
+
+	printf("%d\n", best);
+}
