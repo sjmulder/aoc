32e8f6f7cb582b364524323d16502ccd66beb46c
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-10 21:41:39 +0100

    2020 day 10 part 1 in C#

---
+++ 2020/day10/solve1-linq.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var nums = File.ReadAllLines(args[0])
+			.Select(int.Parse)
+			.ToList();
+		nums.Insert(0, 0);
+		nums.Add(nums.Max() + 3);
+		nums.Sort();
+
+		Console.WriteLine(nums
+			.Zip(nums.Skip(1), (a, b) => b-a)
+			.Where(d => d==1 || d==3)
+			.GroupBy(d => d)
+			.Select(g => g.Count())
+			.Aggregate(1, (acc, x) => acc*x));
+	}
+}
