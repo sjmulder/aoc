3511b7ea0cd197415b8862f9395fd05d936a1327
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-17 11:36:51 +0100

    Optimize

--- 2020/day17/solve2.c
+++ 2020/day17/solve2.c
@@ -6,7 +6,6 @@
 #define SZ 30
 
 static char gr[SZ][SZ][SZ][SZ];
-static long non;
 
 static void
 parse(void)
@@ -17,7 +16,7 @@ parse(void)
 
 	for (; (c = getchar()) != EOF; x++)
 		if (c == '#')
-			{ gr[w][z][y][x] = 1; non++; }
+			gr[w][z][y][x] = 1;
 		else if (c == '\n')
 			{ x = SZ/2-1; y++; }
 }
@@ -33,33 +32,37 @@ step(void)
 	for (dw=-1; dw<2; dw++)
 	for (dz=-1; dz<2; dz++)
 	for (dy=-1; dy<2; dy++)
-	for (dx=-1; dx<2; dx++)
-	for (w=1; w<SZ-1; w++)
-	for (z=1; z<SZ-1; z++)
-	for (y=1; y<SZ-1; y++)
-	for (x=1; x<SZ-1; x++)
-		if ((dx||dy||dz||dw) && gr[w+dw][z+dz][y+dy][x+dx])
-			nadj[w][z][y][x]++;
+	for (dx=-1; dx<2; dx++) {
+		if (!dx && !dy && !dz && !dw)
+			continue;
+		for (w=1; w<SZ-1; w++)
+		for (z=1; z<SZ-1; z++)
+		for (y=1; y<SZ-1; y++)
+		for (x=1; x<SZ-1; x++)
+			nadj[w][z][y][x] += gr[w+dw][z+dz][y+dy][x+dx];
+	}
 
 	for (w=1; w<SZ-1; w++)
 	for (z=1; z<SZ-1; z++)
 	for (y=1; y<SZ-1; y++)
 	for (x=1; x<SZ-1; x++)
-		if ((nadj[w][z][y][x] == 2 && gr[w][z][y][x]) ||
-		     nadj[w][z][y][x] == 3) {
-			if (gr[w][z][y][x])
-				continue;
-			if (w==1 || z==1 || y==1 || x==1 ||
-			    w==SZ-2 || z==SZ-2 || y==SZ-2 || x==SZ-2)
-				errx(1, "going out of bounds");
-			gr[w][z][y][x] = 1;
-			non++;
-		} else {
-			if (!gr[w][z][y][x])
-				continue;
-			gr[w][z][y][x] = 0;
-			non--;
-		}
+		gr[w][z][y][x] =
+		    (nadj[w][z][y][x] == 2 && gr[w][z][y][x]) ||
+		     nadj[w][z][y][x] == 3;
+}
+
+static int
+counton(void)
+{
+	int x,y,z,w, non=0;
+
+	for (w=1; w<SZ-1; w++)
+	for (z=1; z<SZ-1; z++)
+	for (y=1; y<SZ-1; y++)
+	for (x=1; x<SZ-1; x++)
+		non += gr[w][z][y][x];
+
+	return non;
 }
 
 int
@@ -68,9 +71,7 @@ main()
 	int i;
 
 	parse();
-
 	for (i=0; i<6; i++)
 		step();
-
-	printf("%ld\n", non);
+	printf("%d\n", counton());
 }
