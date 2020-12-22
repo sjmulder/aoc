f4c776b39da19a554037166f3e3850f91ca6403b
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-23 00:14:36 +0100

    2020 day 15 in JS

---
+++ 2020/day15/solve.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const map = new Map();
+const input = fs.readFileSync('input', 'utf-8').split(',');
+let part1, cur, prev=0 ;
+
+for (let turn=0; turn < 30000000; turn++) {
+	if (turn < input.length)
+		cur = parseInt(input[turn], 10);
+	else if ((cur = map.get(prev) || 0))
+		cur = turn - cur;
+	map.set(prev, turn);
+	prev = cur;
+	if (turn == 2019) part1 = cur;
+}
+
+console.log(part1, cur);
