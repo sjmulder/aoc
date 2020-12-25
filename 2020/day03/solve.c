f5751b3f1611e556ba0ba467b85efd6db06321b8
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-25 17:18:46 +0100

    Port 2020 day 3 to VC++6

--- 2020/day03/solve.c
+++ 2020/day03/solve.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
+#include "../compat/stdint.h"
+#include "../compat/inttypes.h"
 
 static char buf[1024*1024];
-static size_t sz, w;
+static int sz, w;
 
-static size_t f(int dx, int dy) {
-	size_t i,n=0;
+static int64_t
+f(int dx, int dy)
+{
+	int i;
+	int64_t n=0;
+
 	for (i=0; i*dy*(w+1) < sz-w; i++)
 		n += buf[i*dy*(w+1)+(i*dx%w)] == '#';
+	
 	return n;
 }
 
-int main() {
-	sz = (size_t)fread(buf, 1, sizeof(buf), stdin);
-	w = (size_t)(strchr(buf, '\n') - buf);
-	printf("%zu %zu\n", f(3,1), f(1,1)*f(3,1)*f(5,1)*f(7,1)*f(1,2));
+int
+main(int argc, char **argv)
+{
+	FILE *in;
+
+	in = argc<2 ? stdin : fopen(argv[1], "r");
+	assert(in);
+	sz = fread(buf, 1, sizeof(buf), in);
+	w = (int)(strchr(buf, '\n') - buf);
+
+	printf("%" PRId64 " %" PRId64 "\n",
+	     f(3,1),
+	     f(1,1)*f(3,1)*f(5,1)*f(7,1)*f(1,2));
+	//getchar();
+	return 0;
 }
