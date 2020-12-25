35fa7c2767604134acb352283235fd4d38011c89
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-26 00:34:27 +0100

    Remove old 2020 day 10 solutions

--- 2020/day10/solve-golf.c
+++ 2020/day10/solve-golf.c
@@ -1,4 +1,5 @@
-long p[128];n[128],c[4],i,j,z;C(int*a,int*b){return*a-*b;}main(){while(~
-scanf("%d",++z+n));for(qsort(n,z,sizeof*n,C);++i<z;)c[n[i]-n[i-1]]++;for
-(p[i=z-1]=1;i--;)for(j=i;++j<z&&n[j]-n[i]<4;)p[i]+=p[j];printf("%d %ld\
-\n",c[1]*++c[3],*p);}
+/* originally by e_blake:
+   https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gfaojup/ */
+long c=1,n[200]={1};i,j,o,t,r;main(){while(scanf("%d",&i)>0)n[i]=1;for(;
+j<=200;)n[j++]?!n[j]?o+=r,c*=r["11247"]-48,r=!++t:r++:0;printf("%d %ld",
+o*t,c);}
