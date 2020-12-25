d139113e64d463f09d51d22a038fd5f2f756ec49
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-25 22:41:37 +0100

    Port 2020 day 5 to VC++6

--- 2020/day05/solve.c
+++ 2020/day05/solve.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
+#include <assert.h>
 
-int main() {
+int
+main(int argc, char **argv)
+{
 	static char map[128*8];
+	FILE *f;
 	int c,i, n=0, max=0;
 
-	while ((c = getchar()) != EOF)
+	f = argc<2 ? stdin : fopen(argv[1], "r");
+	assert(f);
+
+	while ((c = fgetc(f)) != EOF)
 		if (c=='\n') { map[n] = 1; if (n>max) max=n; n = 0; }
 		else         { n = n<<1 | (c=='B') | (c=='R'); }
 
 	for (i=1; i < 128*8-1; i++)
 		if (map[i-1] && !map[i] && map[i+1])
 			{ printf("%d %d\n", max, i); break; }
+
+	getchar();
+	return 0;
 }
