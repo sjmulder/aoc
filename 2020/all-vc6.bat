8cd9be30f715789242a337954f487383e1aed6c4
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-26 04:26:14 +0100

    Script to run all, VC++6

---
+++ 2020/all-vc6.bat
@@ -0,0 +1,26 @@
+day01\Release\day01.exe day01\input.txt
+day02\Release\day02.exe day02\input.txt
+day03\Release\day03.exe day03\input.txt
+day04\Release\day04.exe day04\input.txt
+day05\Release\day05.exe day05\input.txt
+day06\Release\day06.exe day06\input.txt
+day07\Release\day07.exe day07\input.txt
+day08\Release\day08.exe day08\input.txt
+day09\Release\day09.exe day09\input.txt
+day10\Release\day10.exe day10\input.txt
+day11\Release\day11.exe day11\input.txt
+day12\Release\day12.exe day12\input.txt
+day13\Release\day13.exe day13\input.txt
+day14\Release\day14.exe day14\input.txt
+day15\Release\day15.exe day15\input.txt
+day16\Release\day16.exe day16\input.txt
+day17\Release\day17.exe day17\input.txt
+day18\Release\day18.exe day18\input.txt
+day19\Release\day19.exe day19\input.txt
+day19\Release\day19.exe day19\input-p2.txt
+day20\Release\day20.exe day20\input.txt
+day21\Release\day21.exe day21\input.txt
+day22\Release\day22.exe day22\input.txt
+day23\Release\day23.exe
+day24\Release\day24.exe day24\input.txt
+day25\Release\day25.exe
