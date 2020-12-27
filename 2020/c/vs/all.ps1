b210fcad94874ecd6e306d41ea7ad411581ca804
Sijmen J. Mulder <ik@sjmulder.nl>
2020-12-27 15:07:35 +0100

    Run script for modern Visual C++

---
+++ 2020/c/vs/all.ps1
@@ -0,0 +1,26 @@
+& ".\bin\$args\day01.exe" ..\..\data\01-input.txt
+& ".\bin\$args\day02.exe" ..\..\data\02-input.txt
+& ".\bin\$args\day03.exe" ..\..\data\03-input.txt
+& ".\bin\$args\day04.exe" ..\..\data\04-input.txt
+& ".\bin\$args\day05.exe" ..\..\data\05-input.txt
+& ".\bin\$args\day06.exe" ..\..\data\06-input.txt
+& ".\bin\$args\day07.exe" ..\..\data\07-input.txt
+& ".\bin\$args\day08.exe" ..\..\data\08-input.txt
+& ".\bin\$args\day09.exe" ..\..\data\09-input.txt
+& ".\bin\$args\day10.exe" ..\..\data\10-input.txt
+& ".\bin\$args\day11.exe" ..\..\data\11-input.txt
+& ".\bin\$args\day12.exe" ..\..\data\12-input.txt
+& ".\bin\$args\day13.exe" ..\..\data\13-input.txt
+& ".\bin\$args\day14.exe" ..\..\data\14-input.txt
+& ".\bin\$args\day15.exe" ..\..\data\15-input.txt
+& ".\bin\$args\day16.exe" ..\..\data\16-input.txt
+& ".\bin\$args\day17.exe" ..\..\data\17-input.txt
+& ".\bin\$args\day18.exe" ..\..\data\18-input.txt
+& ".\bin\$args\day19.exe" ..\..\data\19-input.txt
+& ".\bin\$args\day19.exe" ..\..\data\19-p2-input.txt
+& ".\bin\$args\day20.exe" ..\..\data\20-input.txt
+& ".\bin\$args\day21.exe" ..\..\data\21-input.txt
+& ".\bin\$args\day22.exe" ..\..\data\22-input.txt
+& ".\bin\$args\day23.exe"
+& ".\bin\$args\day24.exe" ..\..\data\24-input.txt
+& ".\bin\$args\day25.exe"
