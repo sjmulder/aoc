b128f7dfa5d564530cf67ff5f555e68cabbf13fe
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-01 13:49:13 +0100

    Similar optimisation in C#

--- 2021/cs/Day01/Program.cs
+++ 2021/cs/Day01/Program.cs
@@ -4,11 +4,8 @@ using System.Linq;
 
 var nums = File.ReadAllLines("../../../../../data/01-input.txt")
     .Select(int.Parse).ToList();
-var pairs = nums
-    .Zip(nums.Skip(1), (a, b) => a + b)
-    .Zip(nums.Skip(2), (a, b) => a + b).ToList();
 
 var inc1 = nums.Zip(nums.Skip(1), (a, b) => a < b).Count(x => x);
-var inc3 = pairs.Zip(pairs.Skip(1), (a, b) => a < b).Count(x => x);
+var inc3 = nums.Zip(nums.Skip(3), (a, b) => a < b).Count(x => x);
 
 Console.WriteLine($"{inc1} {inc3}");
