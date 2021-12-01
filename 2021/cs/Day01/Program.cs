c6293a0bcd8c4729a9cfc0264178621052d894d6
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-01 11:30:49 +0100

    2021 day 1 with C#

---
+++ 2021/cs/Day01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var nums = File.ReadAllLines("../../../../../data/01-input.txt")
+    .Select(int.Parse).ToList();
+var pairs = nums
+    .Zip(nums.Skip(1), (a, b) => a + b)
+    .Zip(nums.Skip(2), (a, b) => a + b).ToList();
+
+var inc1 = nums.Zip(nums.Skip(1), (a, b) => a < b).Count(x => x);
+var inc3 = pairs.Zip(pairs.Skip(1), (a, b) => a < b).Count(x => x);
+
+Console.WriteLine($"{inc1} {inc3}");
