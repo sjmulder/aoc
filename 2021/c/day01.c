5321c9f99db4f0269557ef6c4e747609dce20714
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-01 22:47:28 +0100

    Ring buffer instead of memmove

--- 2021/c/day01.c
+++ 2021/c/day01.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-#include <string.h>
+
+/*
+ * a[] is a 4-element ring buffer. Multiplication is used instead of
+ * '&&' to make this branchless. The '(n+3)%4' actually means '(n-1)%4'
+ * but that would yield some negative indices.
+ */
 
 int
 main()
 {
 	int a[4] = {0}, n=0, ninc1=0, ninc3=0;
 
-	for (; scanf(" %d", a) == 1; n++) {
-		ninc1 += (n>0) * (a[0] > a[1]);
-		ninc3 += (n>2) * (a[0] > a[3]);
-		memmove(a+1, a, sizeof(int)*3);
+	for (; scanf(" %d", &a[n%4]) == 1; n++) {
+		ninc1 += (n>0) * (a[n%4] > a[(n+3)%4]);
+		ninc3 += (n>2) * (a[n%4] > a[(n+1)%4]);
 	}
 
 	printf("%d %d\n", ninc1, ninc3);
