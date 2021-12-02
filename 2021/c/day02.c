ff782882bb66d0520c33b6d457d9e7212dd92ad4
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-02 10:05:00 +0100

    2021 day 2

---
+++ 2021/c/day02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int
+main()
+{
+	/* d is pt.2 depth, a is pt.1 depth AND aim */
+	char cmd[16];
+	int x, h=0,d=0,a=0;
+
+	while (scanf(" %15s %d", cmd, &x) == 2)
+		switch (cmd[0]) {
+		case 'd': a+=x; break;
+		case 'u': a-=x; break;
+		case 'f': h+=x; d+=a*x; break;
+		default: fprintf(stderr, "bad input\n"); return 1;
+		}
+
+	printf("%d %d\n", h*a, h*d);
+}
