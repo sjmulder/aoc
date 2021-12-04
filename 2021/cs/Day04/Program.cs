bc00a23b3bb65cc01c78edf1e37f28aa1173796b
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-04 09:53:39 +0100

    s,board,card,

--- 2021/cs/Day04/Program.cs
+++ 2021/cs/Day04/Program.cs
@@ -1,10 +1,10 @@
-﻿// Boards are represented as int[][] - an array of rows.
+﻿// Cards are represented as int[][] - an array of rows.
 //
 // Bingo checking doesn't work by striking off numbers. Instead rows and columns
 // (transposed rows) are mapped into the last turn (draw index) any of their
 // numbers are drawn, i.e. when they are completed.
 //
-// Then the boards are sorted by their first completed row or column to
+// Then the cards are sorted by their first completed row or column to
 // determine the completion order.
 //
 // Finally the scores are determined by filtering out the numbers drawn before
@@ -16,12 +16,12 @@ var draws = lines.First().Split(',').Select(int.Parse).ToArray();
 var winners = lines.Skip(1).Where(l => l != string.Empty)
     .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries)
         .Select(int.Parse).ToArray()).Chunk(5)
-    .Select(board => (board, drawIdx: board
-        .Concat(Enumerable.Range(0, 5).Select(c => board.Select(r => r[c])))
+    .Select(card => (card, drawIdx: card
+        .Concat(Enumerable.Range(0, 5).Select(c => card.Select(r => r[c])))
         .Select(r => r.Select(x => Array.IndexOf(draws, x)).Max()).Min()))
     .OrderBy(t => t.drawIdx).ToArray();
 var scores = new[] { winners.First(), winners.Last() }
-    .Select(t => t.board.SelectMany(r => r)
+    .Select(t => t.card.SelectMany(r => r)
         .Where(x => !draws.Take(t.drawIdx + 1).Contains(x))
         .Sum() * draws[t.drawIdx]);
 foreach (var score in scores)
