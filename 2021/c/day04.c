ef491065ca57196c5b6eb3c0787ba9ac519586ce
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-04 09:42:08 +0100

    Inline score()

--- 2021/c/day04.c
+++ 2021/c/day04.c
@@ -6,27 +6,14 @@
 #define LEN(a) (sizeof(a)/sizeof(*(a)))
 #define SZ 5
 
-static int draws[100];
-static int boards[100][SZ][SZ], struck[100][SZ][SZ], won[100];
-static int ndraws, nboards, nwon;
-
-static int
-score(int board, int lastdraw)
-{
-	int sum=0, x,y;
-
-	for (y=0; y<SZ; y++)
-	for (x=0; x<SZ; x++)
-		sum += !struck[board][y][x] * boards[board][y][x];
-
-	return sum * lastdraw;
-}
-
 int
 main()
 {
+	static int draws[100];
+	static int boards[100][SZ][SZ], struck[100][SZ][SZ], won[100];
+	int ndraws=0, nboards=0, nwon=0;
+	int i,j, x,y, x2,y2, score, p1=0,p2=0;
 	char buf[400], *rest, *lf, *field;
-	int i,j, x,y, x2,y2, p1=0,p2=0;
 
 	if (!(rest = fgets(buf, sizeof(buf), stdin)))
 		err(1, NULL);
@@ -59,9 +46,14 @@ main()
 		for (y2=0; y2<SZ && struck[j][y2][x]; y2++) ;
 		if (x2 != SZ && y2 != SZ) continue;
 		won[j] = 1;
-		if (++nwon == 1) p1 = score(j, draws[i]);
-		if (nwon == nboards) { p2 = score(j, draws[i]);
-		    goto end; }
+		if (++nwon > 1 && nwon < nboards) continue;
+		score = 0;
+		for (y2=0; y2<SZ; y2++)
+		for (x2=0; x2<SZ; x2++)
+			score += !struck[j][y2][x2] * boards[j][y2][x2];
+		score *= draws[i];
+		if (nwon == 1) p1 = score;
+		if (nwon == nboards) { p2 = score; goto end; }
 	}
 
 end:
