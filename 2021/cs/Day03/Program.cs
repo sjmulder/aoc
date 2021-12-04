888d17838eef89832b6138561d105b51f38717fe
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-04 07:49:46 +0100

    2021 day 3 in C#

---
+++ 2021/cs/Day03/Program.cs
@@ -0,0 +1,23 @@
+﻿int FromBase2(IEnumerable<char> s) => Convert.ToInt32(new string(s.ToArray()), 2);
+bool MostlyOnes(IList<string> l, int bit) => l.Count(s => s[bit] == '1') >= (l.Count + 1) / 2;
+
+var ls = File.ReadAllLines("../../../../../data/03-input.txt");
+var nbits = ls[0].Length;
+var bits = Enumerable.Range(0, ls[0].Length);
+var gamma = FromBase2(bits.Select(bit => MostlyOnes(ls, bit) ? '1' : '0'));
+var epsilon = ~gamma & ((1 << nbits) - 1);
+
+int Part2(bool flip)
+{
+    var list = ls.ToList();
+    for (int bit = 0; bit < nbits && list.Count > 1; bit++)
+    {
+        char val = MostlyOnes(list, bit) != flip ? '1' : '0';
+        list.RemoveAll(x => x[bit] != val);
+    }
+
+    return FromBase2(list[0]);
+}
+
+Console.WriteLine(gamma * epsilon);
+Console.WriteLine(Part2(false) * Part2(true));
