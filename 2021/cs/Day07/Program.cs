9566e45298b551eaab5197741a461b23c01890a4
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-07 14:48:02 +0100

    Tweak

--- 2021/cs/Day07/Program.cs
+++ 2021/cs/Day07/Program.cs
@@ -2,8 +2,6 @@
     .Split(",").Select(int.Parse).OrderBy(x => x).ToArray();
 int p1 = a.Sum(x => Math.Abs(a[a.Length/2]-x));
 int p2 = Enumerable.Range((int)Math.Floor(a.Average()), 2)
-    .Select(pos => a
-        .Select(x => Math.Abs(pos-x))
-        .Select(x => x*(x+1)/2).Sum())
+    .Select(pos => a.Sum(x => { int d = Math.Abs(pos-x); return d*(d+1)/2; }))
     .Min();
 Console.WriteLine($"{p1} {p2}");
