023a23ade31cf88ad42e6b9bd89f426f60732351
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-07 10:28:33 +0100

    2021 day 7 in C#

---
+++ 2021/cs/Day07/Program.cs
@@ -0,0 +1,9 @@
+﻿var a = File.ReadAllText("../../../../../data/07-input.txt")
+    .Split(",").Select(int.Parse).ToArray();
+int min = a.Min(), max = a.Max();
+int p1 = Enumerable.Range(min, max-min+1).Select(pos => a
+    .Select(x => Math.Abs(pos-x)).Sum()).Min();
+int p2 = Enumerable.Range(min, max-min+1).Select(pos => a
+    .Select(x => Math.Abs(pos-x))
+    .Select(x => x*(x+1)/2).Sum()).Min();
+Console.WriteLine($"{p1} {p2}");
