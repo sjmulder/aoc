063ad448031e63fc077f30ac6ead159b9e328052
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-08 15:21:18 +0100

    Tweak

--- 2021/cs/Day07/Program.cs
+++ 2021/cs/Day07/Program.cs
@@ -1,7 +1,7 @@
 ﻿var a = File.ReadAllText("../../../../../data/07-input.txt")
     .Split(",").Select(int.Parse).OrderBy(x => x).ToArray();
 int p1 = a.Sum(x => Math.Abs(a[a.Length/2]-x));
-int p2 = Enumerable.Range((int)Math.Floor(a.Average()), 2)
+int p2 = Enumerable.Range((int)a.Average(), 2)
     .Select(pos => a.Sum(x => { int d = Math.Abs(pos-x); return d*(d+1)/2; }))
     .Min();
 Console.WriteLine($"{p1} {p2}");
