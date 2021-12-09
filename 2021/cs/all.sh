1a250b750042b870f56032d45a16d892e4473dda
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-09 20:27:56 +0100

    Name style

--- 2021/cs/all.sh
+++ 2021/cs/all.sh
@@ -1,8 +1,8 @@
-(cd Day01 && dotnet run -v m --nologo -c Release --no-build) &
-(cd Day02 && dotnet run -v m --nologo -c Release --no-build) &
-(cd Day03 && dotnet run -v m --nologo -c Release --no-build) &
-(cd Day04 && dotnet run -v m --nologo -c Release --no-build) &
-(cd Day06 && dotnet run -v m --nologo -c Release --no-build) &
-(cd Day07 && dotnet run -v m --nologo -c Release --no-build) &
-(cd Day08 && dotnet run -v m --nologo -c Release --no-build) &
+(cd day01 && dotnet run -v m --nologo -c Release --no-build) &
+(cd day02 && dotnet run -v m --nologo -c Release --no-build) &
+(cd day03 && dotnet run -v m --nologo -c Release --no-build) &
+(cd day04 && dotnet run -v m --nologo -c Release --no-build) &
+(cd day06 && dotnet run -v m --nologo -c Release --no-build) &
+(cd day07 && dotnet run -v m --nologo -c Release --no-build) &
+(cd day08 && dotnet run -v m --nologo -c Release --no-build) &
 wait
