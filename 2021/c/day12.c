44983bc70b8169bb13788963ab98d96812d607f6
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-12 11:42:21 +0100

    Clean up

--- 2021/c/day12.c
+++ 2021/c/day12.c
@@ -3,62 +3,43 @@
 #include <string.h>
 #include <assert.h>
 
-#define LEN(a)  (sizeof(a)/sizeof(*(a)))
-#define BIG(id) (*(char*)&id < 'a')
+/*
+ * Implemented as a depth-first search using recursion. The route is
+ * kept in a linked list pointing *back* to the visited notes. This
+ * makes managing the list trivial - each level of recursion just keeps
+ * its own link on the stack.
+ *
+ * To avoid a bunch of strcmp(), nodes are identified by interpreting
+ * the first two bytes of their name strings as 16-bit number.
+ */
 
-#define START (*(uint16_t*)"start")
-#define END   (*(uint16_t*)"end")
+#define LEN(a)	(sizeof(a)/sizeof(*(a)))
+#define BIG(id)	(*(char*)&id < 'a')
+
+#define START	(*(uint16_t*)"start")
+#define END	(*(uint16_t*)"end")
 
 struct step;
-
 struct link { uint16_t a, b; };
 struct step { struct step *prev; uint16_t id; };
 
 static struct link ls[32];
 static int nls;
 
-static void
-dump_links(void)
-{
-	int i;
-
-	for (i=0; i<nls; i++)
-		printf("%.2s-%.2s\n", (char*)&ls[i].a,
-		    (char*)&ls[i].b);
-}
-
-static void
-dump_route(struct step *step)
-{
-	for (; step; step = step->prev)
-		printf("%.2s,", (char*)&step->id);
-
-	putchar('\n');
-}
-
 static int
 search(struct step *prev, int maytwice)
 {
-	struct step *s;
-	struct step next;
+	struct step next, *s;
 	int count=0, nvisits, i;
 
 	next.prev = prev;
-
-	//printf("searching ");
-	//dump_route(prev);
 
 	for (i=0; i<nls; i++) {
 		if (ls[i].a == prev->id) next.id = ls[i].b; else
 		if (ls[i].b == prev->id) next.id = ls[i].a; else
 			continue;
 		if (next.id == START) continue;
-		if (next.id == END) {
-			//printf("found ");
-			//dump_route(&next);
-			count++;
-			continue;
-		}
+		if (next.id == END) { count++; continue; }
 
 		if (BIG(next.id))
 			count += search(&next, maytwice);
