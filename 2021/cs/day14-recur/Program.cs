7c0b697024513901a2d3ee8dbfd0d058e15e0960
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-15 00:21:55 +0100

    Brute force 2021 day 14 'solution' in C#

---
+++ 2021/cs/day14-recur/Program.cs
@@ -0,0 +1,20 @@
+﻿// Brute force 'solution' that runs in O(nsteps) space - good - but O(2^nsteps)
+// time - very bad. But it's elegant and that's what really matters!
+
+var lines = File.ReadAllLines("../../data/14-input.txt");
+
+var rules = lines.Skip(2)
+    .Select(l => l.Split(" -> "))
+    .ToDictionary(a => (a[0][0], a[0][1]), a => a[1][0]);
+
+IEnumerable<char> Step(IEnumerable<char> chars)
+    => chars.Intersperse((left, right) => rules[(left, right)]);
+
+IEnumerable<char> Steps(IEnumerable<char> chars, int count)
+    => Enumerable.Range(0, count).Aggregate(chars, (acc, _) => Step(acc));
+
+long Score(IEnumerable<char> chars)
+    => chars.ToFrequencyTable().Values.Spread();
+
+Console.WriteLine("14 (p1): " + Score(Steps(lines[0], 10)));
+Console.WriteLine("14 (p2): " + Score(Steps(lines[0], 40)));
---
+++ 2021/cs/day14-recur/Util.cs
@@ -0,0 +1,35 @@
+﻿static class Util
+{
+    public static IEnumerable<T> Intersperse<T>(
+        this IEnumerable<T> elems, Func<T, T, T> selector)
+    {
+        using var iter = elems.GetEnumerator();
+
+        if (!iter.MoveNext())
+            yield break;
+
+        T prev = iter.Current;
+        yield return iter.Current;
+
+        while (iter.MoveNext())
+        {
+            yield return selector(prev, iter.Current);
+            yield return iter.Current;
+            prev = iter.Current;
+        }
+    }
+
+    public static Dictionary<T, long> ToFrequencyTable<T>(
+        this IEnumerable<T> elems)
+    {
+        var table = new Dictionary<T, long>();
+
+        foreach (var x in elems)
+            table[x] = table.GetValueOrDefault(x, 0) + 1;
+
+        return table;
+    }
+
+    public static long Spread(this IEnumerable<long> nums)
+        => nums.Max() - nums.Min();
+}
