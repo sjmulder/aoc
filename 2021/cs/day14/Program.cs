c669c3f9ae8e128b8a29d4bf20c1b6944425f410
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-15 01:07:43 +0100

    2021 day 14 in not so pretty C# with Linq

---
+++ 2021/cs/day14/Program.cs
@@ -0,0 +1,41 @@
+﻿var lines = File.ReadAllLines("../../data/14-input.txt");
+
+var rules = lines.Skip(2)
+    .Select(l => l.Split(" -> "))
+    .ToDictionary(a => (a[0][0], a[0][1]), a => a[1][0]);
+
+var charCounts = lines[0]
+    .GroupBy(x => x).Select(gp => (ch: gp.Key, count: gp.LongCount()))
+    .ToArray();
+
+var pairCounts = lines[0].Zip(lines[0].Skip(1), (a, b) => (a, b))
+    .GroupBy(x => x).Select(gp => (pair: gp.Key, count: gp.LongCount()))
+    .ToArray();
+
+long p1 = 0;
+
+for (int step = 0; step < 40; step++)
+{
+    charCounts = charCounts
+        .Concat(pairCounts.Select(pc => (ch: rules[pc.pair], pc.count)))
+        .GroupBy(cc => cc.ch)
+        .Select(gp => (ch: gp.Key, count: gp.Sum(cc => cc.count)))
+        .ToArray();
+
+    pairCounts = pairCounts
+        .SelectMany(pc => new[]
+        {
+            (pair: (pc.pair.a, rules[pc.pair]), count: pc.count),
+            (pair: (rules[pc.pair], pc.pair.b), count: pc.count)
+        })
+        .GroupBy(pc => pc.pair)
+        .Select(gp => (pc: gp.Key, count: gp.Sum(pc => pc.count)))
+        .ToArray();
+
+    if (step == 9)
+        p1 = charCounts.Select(cc => cc.count).Spread();
+}
+
+long p2 = charCounts.Select(cc => cc.count).Spread();
+
+Console.WriteLine($"14: {p1} {p2}");
---
+++ 2021/cs/day14/Util.cs
@@ -0,0 +1,5 @@
+﻿static class Util
+{
+    public static long Spread(this IEnumerable<long> nums)
+        => nums.Max() - nums.Min();
+}
