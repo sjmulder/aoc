7ae85fc8fa4266add4e66626dbbc33675f66fff0
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-17 11:56:29 +0100

    Naming

--- 2021/c/day17.c
+++ 2021/c/day17.c
@@ -4,20 +4,21 @@
 int
 main()
 {
-	int tx0,tx1,ty0,ty1, vx0,vy0,vx,vy,x,y, p1,p2=0;
+	int left,right,bottom,top, vx0,vy0,vx,vy,x,y, p1,p2=0;
 
-	scanf("target area: x=%d..%d, y=%d..%d", &tx0,&tx1,&ty0,&ty1);
+	scanf("target area: x=%d..%d, y=%d..%d",
+	    &left, &right, &bottom, &top);
 
-	p1 = ty0 * (ty0+1) /2;
+	p1 = bottom * (bottom+1) /2;
 
-	for (vx0=(int)sqrt(tx0)/2; vx0<=tx1; vx0++)
-	for (vy0=ty0; vy0<-ty0; vy0++) {
+	for (vx0 = (int)sqrt(left)/2; vx0 <= right; vx0++)
+	for (vy0 = bottom; vy0 < -bottom; vy0++) {
 		x=y=0; vx=vx0; vy=vy0;
-		while ((x<tx0 || y>ty1) && x<=tx1 && y>=ty0) {
+		while ((x<left || y>top) && x<=right && y>=bottom) {
 			x += vx; vx -= vx>0;
 			y += vy; vy -= 1;
 		}
-		p2 += x>=tx0 && x<=tx1 && y>=ty0 && y<=ty1;
+		p2 += x>=left && x<=right && y>=bottom && y<=top;
 	}
 
 	printf("17: %d %d\n", p1, p2);
