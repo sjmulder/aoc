3d383482622826ad193c17497f91b69d1210ef94
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-18 00:00:01 +0100

    2021 day 17 in C, golfed

---
+++ 2021/c-golf/day17.c
@@ -0,0 +1,3 @@
+l=179,r=201,b=-109,t=-63,m,n,u,v,x,y,a;main(){for(;m++<r;)for(
+n=b-1;++n<-b;a+=x>=l&x<=r&y>=b&y<=t)for(x=y=0,u=m,v=n;(x<l|y>
+t)&x<=r&y>=b;u-=u>0)x+=u,y+=v--;printf("%d %d",b*(b+1)/2,a);}
