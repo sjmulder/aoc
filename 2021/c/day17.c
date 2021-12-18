2bda8107924cc4115117d94cd8eb477d47029804
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-18 18:07:50 +0100

    Naming

--- 2021/c/day17.c
+++ 2021/c/day17.c
@@ -4,21 +4,21 @@
 int
 main()
 {
-	int left,right,bottom,top, vx0,vy0,vx,vy,x,y, p1,p2=0;
+	int l,r,b,t, vx0,vy0,vx,vy,x,y, p1,p2=0;
 
-	scanf("target area: x=%d..%d, y=%d..%d",
-	    &left, &right, &bottom, &top);
+	scanf("target area: x=%d..%d, y=%d..%d", &l,&r,&b,&t);
 
-	p1 = bottom * (bottom+1) /2;
+	p1 = b * (b+1) /2;
 
-	for (vx0 = (int)sqrt(left)/2; vx0 <= right; vx0++)
-	for (vy0 = bottom; vy0 < -bottom; vy0++) {
-		x=y=0; vx=vx0; vy=vy0;
-		while ((x<left || y>top) && x<=right && y>=bottom) {
-			x += vx; vx -= vx>0;
-			y += vy; vy -= 1;
+	for (vx0 = (int)sqrt(l); vx0 <= r; vx0++) {
+		for (vy0=b; vy0<-b; vy0++) {
+			x=y=0; vx=vx0; vy=vy0;
+			while ((x<l || y>t) && x<=r && y>=b) {
+				x += vx; vx -= vx>0;
+				y += vy; vy -= 1;
+			}
+			p2 += x>=l && x<=r && y>=b && y<=t;
 		}
-		p2 += x>=left && x<=right && y>=bottom && y<=top;
 	}
 
 	printf("17: %d %d\n", p1, p2);
