88b373a421c68548ea473e1ecbc0853917cd82b5
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-21 17:05:05 +0100

    Clean up

--- 2021/c/day21.c
+++ 2021/c/day21.c
@@ -6,7 +6,7 @@
 #define MAX(a,b) ((a)>(b)?(a):(b))
 
 struct state { int pos[2], sc[2]; };
-struct p2 { uint64_t wins[2]; };
+struct p2 { uint64_t wins[2]; char set; };
 
 static struct state
 apply_throw(struct state st, int p, int throw)
@@ -43,24 +43,17 @@ solve_p1(struct state st)
 static struct p2
 solve_p2(struct state s, int p)
 {
-	static char incache[10][20][10][20][2];
 	static struct p2 cache[10][20][10][20][2];
-	struct p2 res={};
+	struct p2 res={}, *ent;
 
-	printf("solve_p2(%d,%d, %d,%d, %d)",
-	    s.pos[0],s.sc[0], s.pos[1],s.sc[1], p);
+	ent = &cache[s.pos[0]][s.sc[0]][s.pos[1]][s.sc[1]][p];
 
 	if (s.sc[!p] >= 21) {
-		printf(" win\n");
 		res.wins[!p] = 1;
 		return res;
-	} else if (incache[s.pos[0]][s.sc[0]][s.pos[1]][s.sc[1]][p]) {
-		res = cache[s.pos[0]][s.sc[0]][s.pos[1]][s.sc[1]][p];
-		printf(" in cache (%"PRIu64", %"PRIu64")\n",
-		    res.wins[0], res.wins[1]);
-		return res;
+	} else if (ent->set) {
+		return *ent;
 	} else {
-		printf(" recur\n");
 		res_add(&res, solve_p2(apply_throw(s,p, 3), !p), 1);
 		res_add(&res, solve_p2(apply_throw(s,p, 4), !p), 3);
 		res_add(&res, solve_p2(apply_throw(s,p, 5), !p), 6);
@@ -69,10 +62,8 @@ solve_p2(struct state s, int p)
 		res_add(&res, solve_p2(apply_throw(s,p, 8), !p), 3);
 		res_add(&res, solve_p2(apply_throw(s,p, 9), !p), 1);
 
-		incache[s.pos[0]][s.sc[0]][s.pos[1]][s.sc[1]][p] = 1;
-		cache[s.pos[0]][s.sc[0]][s.pos[1]][s.sc[1]][p] = res;
-
-		return res;
+		res.set = 1;
+		return *ent = res;
 	}
 }
 
