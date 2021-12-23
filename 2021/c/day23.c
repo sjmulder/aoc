192a4139badf896579724119717235b272c322e7
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-23 22:40:15 +0100

    Clean up

--- 2021/c/day23.c
+++ 2021/c/day23.c
@@ -29,30 +29,6 @@ static int dists[4][7] = {
 	{9,8,6,4,2,2,3}
 };
 
-static void
-dump(struct state s)
-{
-	int i;
-
-	printf("%c%c.%c.%c.%c.%c%c",
-	    s.halls[0] ? s.halls[0] : '.',
-	    s.halls[1] ? s.halls[1] : '.',
-	    s.halls[2] ? s.halls[2] : '.',
-	    s.halls[3] ? s.halls[3] : '.',
-	    s.halls[4] ? s.halls[4] : '.',
-	    s.halls[5] ? s.halls[5] : '.',
-	    s.halls[6] ? s.halls[6] : '.');
-	
-	for (i=0; i<s.nslots; i++)
-		printf("\n  %c %c %c %c",
-		    s.rooms[0][i] ? s.rooms[0][i] : '.',
-		    s.rooms[1][i] ? s.rooms[1][i] : '.',
-		    s.rooms[2][i] ? s.rooms[2][i] : '.',
-		    s.rooms[3][i] ? s.rooms[3][i] : '.');
-
-	printf("  %d\n\n", s.score);
-}
-
 /* checks if OCCUPIED slots of room are correct */
 static int
 check_room(struct state s, int room)
@@ -85,27 +61,14 @@ check_route(struct state s, int room, int hall)
 {
 	int roomx, hallx, i;
 
+	/* fudge coordinates so that rooms are *inbetween* halls */
 	roomx = room*2 +3;
 	hallx = hall*2;
 
-	//printf("room=%d, hall=%d,  roomx=%d, hallx=%d\n", room, hall,
-	//    roomx, hallx);
-
-	for (i = MIN(roomx,hallx)+1; i < MAX(roomx,hallx); i++) {
-		//printf(" i=%d halls[i/2]=%c\n", i,
-		//    s.halls[i/2] ? s.halls[i/2] : '.');
-
-		if (!(i%2) && s.halls[i/2]) {
-			//printf(" returning 0\n");
+	for (i = MIN(roomx, hallx)+1; i < MAX(roomx, hallx); i++)
+		if (!(i%2) && s.halls[i/2])
 			return 0;
-		}
-	}
 
-	//for (i = MIN(room+2, hall+1); i < MAX(room+2, hall); i++)
-	//	if (s.halls[i])
-	//		return 0;
-	
-	//printf(" returning 1\n");
 	return 1;
 }
 
@@ -158,11 +121,8 @@ solve(struct state s, int reset)
 
 	if (s.score >= best)
 		return best;
-	//dump(s);
-	if (check_win(s)) {
-		//printf("%.*s END\n", depth, spaces);
+	if (check_win(s))
 		return s.score;
-	}
 
 	/* hall -> room */
 	for (i=0; i<4; i++)
@@ -180,9 +140,6 @@ solve(struct state s, int reset)
 			continue;
 
 		for (k=s.nslots-1; s.rooms[i][k]; k--) ;
-
-		//printf("%.*s hall %d -> room %d.%d\n", depth, spaces,
-		//    j,i,k);
 
 		depth++;
 		best = solve(move_in(s,i,j,k), 0);
@@ -203,9 +160,6 @@ solve(struct state s, int reset)
 
 		for (k=0; !s.rooms[i][k]; k++) ;
 
-		//printf("%.*s room %d.%d -> hall %d\n", depth, spaces,
-		//    i,k,j);
-
 		depth++;
 		best = solve(move_out(s,i,j,k), 0);
 		depth--;
@@ -220,52 +174,24 @@ main()
 	struct state s={};
 	int n,i, p1=0,p2=0;
 
-#if 1
 	n = scanf(" %*s %*s ###%c#%c#%c#%c### #%c#%c#%c#%c",
 	    &s.rooms[0][0], &s.rooms[1][0],
 	    &s.rooms[2][0], &s.rooms[3][0],
 	    &s.rooms[0][1], &s.rooms[1][1],
 	    &s.rooms[2][1], &s.rooms[3][1]);
 	assert(n == 8);
-#endif
 
-#if 0
-	s.halls[6] = 'C';
-
-	s.rooms[0][0] = 'A';
-	s.rooms[0][1] = 'A';
-	s.rooms[0][2] = 'A';
-	s.rooms[0][3] = 'A';
-	s.rooms[1][0] = 'B';
-	s.rooms[1][1] = 'B';
-	s.rooms[1][2] = 'B';
-	s.rooms[1][3] = 'B';
-	s.rooms[2][1] = 'C';
-	s.rooms[2][2] = 'C';
-	s.rooms[2][3] = 'C';
-	s.rooms[3][0] = 'D';
-	s.rooms[3][1] = 'D';
-	s.rooms[3][2] = 'D';
-	s.rooms[3][3] = 'D';
-#endif
-
-#if 1
 	s.nslots = 2;
-	//dump(s);
 	p1 = solve(s, 1);
-#endif
 
-#if 1
 	for (i=0; i<4; i++)
 		s.rooms[i][3] = s.rooms[i][1];
 	s.rooms[0][1] = 'D'; s.rooms[0][2] = 'D';
 	s.rooms[1][1] = 'C'; s.rooms[1][2] = 'B';
 	s.rooms[2][1] = 'B'; s.rooms[2][2] = 'A';
 	s.rooms[3][1] = 'A'; s.rooms[3][2] = 'C';
-#endif
 
 	s.nslots = 4;
-	//dump(s);
 	p2 = solve(s, 1);
 
 	printf("23: %d %d\n", p1, p2);
