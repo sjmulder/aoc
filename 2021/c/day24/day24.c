80990199226f278a06e56db4a03f02abeef97b27
Sijmen J. Mulder <ik@sjmulder.nl>
2021-12-25 01:21:58 +0100

    Flexible number of jobs; search upwards

--- 2021/c/day24/day24.c
+++ 2021/c/day24/day24.c
@@ -5,6 +5,8 @@
 #include <signal.h>
 #include <err.h>
 
+#define NJOBS 8		/* 1 to 8 */
+
 int64_t monad(int input[14]);
 
 static void
@@ -12,26 +14,25 @@ solve(int job)
 {
 	int input[14], i;
 
-	input[0] = job+1;
-
-	for (input[1] = 9; input[1] > 0; input[1]--)
-	for (input[2] = 9; input[2] > 0; input[2]--)
-	for (input[3] = 9; input[3] > 0; input[3]--)
-	for (input[4] = 9; input[4] > 0; input[4]--) {
-		for (i=0; i<5; i++)
+	for (input[0] = 1+job; input[0] < 10; input[0] += NJOBS)
+	for (input[1] = 1; input[1] < 10; input[1]++)
+	for (input[2] = 1; input[2] < 10; input[2]++)
+	for (input[3] = 1; input[3] < 10; input[3]++)
+	for (input[4] = 1; input[4] < 10; input[4]++)
+	for (input[5] = 1; input[5] < 10; input[5]++)
+	for (input[6] = 1; input[6] < 10; input[6]++) {
+		for (i=0; i<7; i++)
 			putchar('0' + input[i]);
-		printf("000000000\n");
+		printf("1111111\n");
 		fflush(stdout);
 
-		for (input[5] = 9; input[5] > 0; input[5]--)
-		for (input[6] = 9; input[6] > 0; input[6]--)
-		for (input[7] = 9; input[7] > 0; input[7]--)
-		for (input[8] = 9; input[8] > 0; input[8]--)
-		for (input[9] = 9; input[9] > 0; input[9]--)
-		for (input[10] = 9; input[10] > 0; input[10]--)
-		for (input[11] = 9; input[11] > 0; input[11]--)
-		for (input[12] = 9; input[12] > 0; input[12]--)
-		for (input[13] = 9; input[13] > 0; input[13]--)
+		for (input[7] = 1; input[7] < 10; input[7]++)
+		for (input[8] = 1; input[8] < 10; input[8]++)
+		for (input[9] = 1; input[9] < 10; input[9]++)
+		for (input[10] = 1; input[10] < 10; input[10]++)
+		for (input[11] = 1; input[11] < 10; input[11]++)
+		for (input[12] = 1; input[12] < 10; input[12]++)
+		for (input[13] = 1; input[13] < 10; input[13]++)
 			if (monad(input) == 0) {
 				printf("FOUND ");
 				for (i=0; i<14; i++)
@@ -45,10 +46,10 @@ solve(int job)
 int
 main()
 {
-	pid_t pids[9], pid;
+	pid_t pids[NJOBS], pid;
 	int i, status, ret=0;
 
-	for (i=0; i<9; i++)
+	for (i=0; i<NJOBS; i++)
 		switch ((pids[i] = fork())) {
 		case -1: err(1, "fork");
 		case 0:  solve(i); return 0;
@@ -59,14 +60,14 @@ main()
 			err(1, "wait");
 		if (pid == -1) break;
 
-		for (i=0; i<9; i++)
+		for (i=0; i<NJOBS; i++)
 			if (pids[i] == pid)
 				{ pids[i] = 0; break; }
 
 		printf("%d exited %d\n", pid, WEXITSTATUS(status));
 
 		if (WEXITSTATUS(status)) {
-			for (i=0; i<9; i++)
+			for (i=0; i<NJOBS; i++)
 				if (pids[i] && kill(pids[i], SIGINT))
 					err(1, "kill");
 			break;
