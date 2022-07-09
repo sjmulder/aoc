91098ca1cdae9eac44e57c1200a8c35545bb826a
Sijmen J. Mulder <ik@sjmulder.nl>
2022-07-09 17:00:14 +0200

    Fix invocation

--- 2015/c/all.sh
+++ 2015/c/all.sh
@@ -1,7 +1,7 @@
 ./day01    <../data/01-input.txt &
 ./day02    <../data/02-input.txt &
 echo '03': $(./day03-p1 <../data/03-input.txt) \
-           $(./day03-p1 <../data/03-input.txt) &
+           $(./day03-p2 <../data/03-input.txt) &
 ./day04    ckczppom
 echo '05:' $(./day05-p1 <../data/05-input.txt) \
            $(./day05-p2 <../data/05-input.txt) &
@@ -11,9 +11,10 @@ echo '07:' $(./wiresim a <../data/07-input.txt) \
            $(./wiresim a <../data/07-input-p2.txt) &
 echo '08: not implemented'
 echo '09: not implemented'
-echo '10:' $(./day10 cqjxjnds 40) \
-           $(./day10 cqjxjnds 50) &
-./day11    cqjxjnds
+echo '10:' $(./day10 1113222113 40) \
+           $(./day10 1113222113 50) &
+echo '11:' $(./day11 cqjxjnds) \
+           $(./day11 cqjxxyzz) &
 echo '12:' $(./day12-p1 <../data/12-input.txt) \
            $(./day12-p2 <../data/12-input.txt) &
 ./day13    <../data/13-input.txt &
