19776c0ceeb8c129e5dcf3e30d98aa1260d7ac8a
Sijmen J. Mulder <ik@sjmulder.nl>
2022-07-09 17:16:45 +0200

    Reuse MD5_CTX for better perf

--- 2015/c/day04.c
+++ 2015/c/day04.c
@@ -8,6 +8,7 @@ main(int argc, char **argv) {
 	char s[64];
 	unsigned char d[16];
 	int p1=0, p2=0, i, len;
+	MD5_CTX md5;
 
 	if (argc == 1)
 		errx(1, "need arg"); 
@@ -17,7 +18,10 @@ main(int argc, char **argv) {
 		if (len<1 || len >= (int)sizeof(s))
 			errx(1, "snprintf");
 
-		MD5((unsigned char *)s, len, d);
+		MD5_Init(&md5);
+		MD5_Update(&md5, (unsigned char*)s, len);
+		MD5_Final(d, &md5);
+
 		if (!p1 && !d[0] && !d[1] && !(d[2] & 0xF0))
 			p1 = i;
 		if (!p2 && !d[0] && !d[1] && !d[2])
