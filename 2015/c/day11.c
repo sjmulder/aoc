bac9f00962b18bbf60f62e282b4a30605ef3a125
Sijmen J. Mulder <ik@sjmulder.nl>
2022-07-09 17:00:55 +0200

    Don't consider original input

--- 2015/c/day11.c
+++ 2015/c/day11.c
@@ -9,11 +9,18 @@ main(int argc, char **argv)
 	int i, len, inc, npr;
 
 	if (argc != 2)
-		errx(1, "usage: solve <pass>");
+		errx(1, "usage: day11 <pass>");
 
 	s = argv[1];
 	len = strlen(s);
 	while (1) {
+		for (i=len-1; i>=0 && s[i]=='z'; i--)
+			;
+		if (i == -1)
+			{ puts("11: no solution"); return 0; }
+		memset(&s[i+1], 'a', len-i-1);
+		s[i]++;
+
 		inc=0; npr=0;
 		for (i=0; i<len; i++) {
 			if (strchr("iol", s[i]))
@@ -23,17 +30,7 @@ main(int argc, char **argv)
 			npr += (i==2 && s[i-1]==s[i])
 			    || (i>=3 && s[i-1]==s[i] && s[i-2]!=s[i-1]);
 		}
-		if (inc && npr>=2 && i==len) {
-			printf("11: %s\n", s);
-			return 0;
-		}
-		for (i=len-1; i>=0 && s[i]=='z'; i--)
-			;
-		if (i == -1) {
-			puts("11: no solution");
-			return 0;
-		}
-		memset(&s[i+1], 'a', len-i-1);
-		s[i]++;
+		if (inc && npr>=2 && i==len)
+			{ puts(s); return 0; }
 	}
 }
