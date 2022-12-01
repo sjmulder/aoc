672823c0654d0d3c2eaf63c762731c1f8d52c005
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-01 22:39:38 +0100

    Simplify

--- 2022/c/day01.c
+++ 2022/c/day01.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-#define LEN(a)	(sizeof(a)/sizeof(*(a)))
 
 int
 main()
 {
 	char buf[64], *line;
-	int cals, sum=0, top1=0, top[3]={}, top_sum=0;
-	size_t i;
+	int cals, sum=0, top[3]={};
 
 	do {
 		line = fgets(buf, sizeof(buf), stdin);
@@ -16,21 +12,20 @@ main()
 		if (line && sscanf(line, "%d", &cals) == 1)
 			sum += cals;
 		else {
-			for (i=0; i < LEN(top); i++)
-				if (top[i] < sum) {
-					top_sum += sum - top[LEN(top)-1];
-					memmove(&top[i+1], &top[i],
-					   sizeof(*top) * (LEN(top)-1-i));
-					top[i] = sum;
-					break;
-				}
-			if (top1 < sum)
-				top1 = sum;
+			if (sum > top[0]) {
+				top[2] = top[1];
+				top[1] = top[0];
+				top[0] = sum;
+			} else if (sum > top[1]) {
+				top[2] = top[1];
+				top[1] = sum;
+			} else if (sum > top[2])
+				top[2] = sum;
 			sum = 0;
 		}
 	} while (line);
 
-	printf("%d %d\n", top1, top_sum);
+	printf("%d %d\n", top[0], top[0]+top[1]+top[2]);
 
 	return 0;
 }
