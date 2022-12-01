2ca69953a318e9c7298a31a07b0440ee7f0dbe04
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-01 22:27:25 +0100

    2022 day 1 in C#

---
+++ 2022/cs/day01/Program.cs
@@ -0,0 +1,10 @@
+﻿using day01;
+
+var top = File.ReadLines("../../data/01-input.txt")
+    .GroupBySeparator(string.IsNullOrWhiteSpace)
+    .Select(group => group.Select(int.Parse).Sum())
+    .OrderByDescending(x => x)
+    .Take(3)
+    .ToList();
+    
+Console.WriteLine($"01: {top[0]} {top.Sum()}");
