ecb60f639ebf3cf6cd1cdc19b0bda2a3972aa2d4
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-02 10:14:47 +0100

    2022, day 2

--- 2022/c/all.sh
+++ 2022/c/all.sh
@@ -1,2 +1,3 @@
 ./day01 <../data/01-input.txt &
+./day02 <../data/02-input.txt &
 wait
---
+++ 2022/c/day02.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+main()
+{
+	char a, b;
+	int p1=0, p2=0;
+
+	while (scanf(" %c %c", &a, &b) == 2) {
+		a -= 'A';
+		b -= 'X';
+
+		switch ((b+3-a)%3) {
+		case 0: p1 += b+1 +3; break;
+		case 1: p1 += b+1 +6; break;
+		case 2: p1 += b+1;    break;
+		}
+
+		switch (b) {
+		case 0: p2 += (a+2)%3 +1;    break;
+		case 1: p2 +=  a      +1 +3; break;
+		case 2: p2 += (a+1)%3 +1 +6; break;
+		}
+	}
+
+	printf("02: %d %d\n", p1, p2);
+}
