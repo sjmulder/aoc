ddb0ad3ae16b06712fedb5408fffa8619d962db4
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-04 10:17:01 +0100

    2022 day 4

--- 2022/c/all.sh
+++ 2022/c/all.sh
@@ -1,4 +1,5 @@
 ./day01 <../data/01-input.txt &
 ./day02 <../data/02-input.txt &
 ./day03 <../data/03-input.txt &
+./day04 <../data/04-input.txt &
 wait
---
+++ 2022/c/day04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int
+main()
+{
+	int p1=0, p2=0;
+	int a0,a1, b0,b1;
+
+	while (scanf(" %d-%d,%d-%d", &a0,&a1, &b0,&b1) == 4) {
+		p1 += (a0>=b0 && a1<=b1) || (b0>=a0 && b1<=a1);
+		p2 += (a0>=b0 && a0<=b1) ||
+		      (a1>=b0 && a1<=b1) ||
+		      (b0>=a0 && b0<=a1) ||
+		      (b1>=a0 && b1<=a1);
+	}
+	
+	printf("04: %d %d\n", p1, p2);
+	return 0;
+}
