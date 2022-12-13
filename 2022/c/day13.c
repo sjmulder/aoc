065d41dbd705edd1add17d766bbc4dd12ec7f5ae
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-13 18:32:42 +0100

    Naming

--- 2022/c/day13.c
+++ 2022/c/day13.c
@@ -36,57 +36,55 @@ parse_tok(char *s, char **endp)
 }
 
 static int
-compare_lists(char *ls, char *rs)
+compare_lists(char *a, char *b)
 {
-	struct tok lt, rt;
+	struct tok a_tok, b_tok;
 	char buf[16];
 
-	lt = parse_tok(ls, &ls);
-	rt = parse_tok(rs, &rs);
+	a_tok = parse_tok(a, &a);
+	b_tok = parse_tok(b, &b);
 
-	switch (lt.type) {
+	switch (a_tok.type) {
 	case TOK_INT:
-		switch (rt.type) {
+		switch (b_tok.type) {
 		case TOK_INT:
-			if (lt.val < rt.val) return -1;
-			if (lt.val > rt.val) return  1;
-			return compare_lists(ls, rs);
+			if (a_tok.val < b_tok.val) return -1;
+			if (a_tok.val > b_tok.val) return  1;
+			return compare_lists(a, b);
 		case TOK_LIST_BEGIN:
-			snprintf(buf, sizeof(buf), "%d]",
-			    lt.val);
-			return compare_lists(buf, rs);
+			snprintf(buf, sizeof(buf), "%d]", a_tok.val);
+			return compare_lists(buf, b);
 		case TOK_LIST_END:
 		case TOK_EOF:
 		default:
 			return 1;
 		}
 	case TOK_LIST_BEGIN:
-		switch (rt.type) {
+		switch (b_tok.type) {
 		case TOK_INT:
-			snprintf(buf, sizeof(buf), "%d]",
-			    rt.val);
-			return compare_lists(ls, buf);
+			snprintf(buf, sizeof(buf), "%d]", b_tok.val);
+			return compare_lists(a, buf);
 		case TOK_LIST_BEGIN:
-			return compare_lists(ls, rs);
+			return compare_lists(a, b);
 		case TOK_LIST_END:
 		case TOK_EOF:
 		default:
 			return 1;
 		}
 	case TOK_LIST_END:
-		switch (rt.type) {
+		switch (b_tok.type) {
 		case TOK_INT:
 		case TOK_LIST_BEGIN:
 			return -1;
 		case TOK_LIST_END:
-			return compare_lists(ls, rs);
+			return compare_lists(a, b);
 		case TOK_EOF:
 		default:
 			return 1;
 		}
 	case TOK_EOF:
 	default:
-		switch (rt.type) {
+		switch (b_tok.type) {
 		case TOK_INT:
 		case TOK_LIST_BEGIN:
 		case TOK_LIST_END:
@@ -101,22 +99,22 @@ compare_lists(char *ls, char *rs)
 int
 main()
 {
-	static char buf1[256], buf2[256];
+	static char line1[256], line2[256];
 	int p1=0, dec1=0, dec2=0, i;
 
 	for (i=0; ; i++) {
-		if (!(fgets(buf1, sizeof(buf1), stdin))) break;
-		if (!(fgets(buf2, sizeof(buf2), stdin))) break;
+		if (!(fgets(line1, sizeof(line1), stdin))) break;
+		if (!(fgets(line2, sizeof(line2), stdin))) break;
 
-		if (compare_lists(buf1, buf2) < 0)
+		if (compare_lists(line1, line2) < 0)
 			p1 += i+1;
 
-		dec1 += compare_lists(buf1, "[[2]]") < 0;
-		dec1 += compare_lists(buf2, "[[2]]") < 0;
-		dec2 += compare_lists(buf1, "[[6]]") < 0;
-		dec2 += compare_lists(buf2, "[[6]]") < 0;
+		dec1 += compare_lists(line1, "[[2]]") < 0;
+		dec1 += compare_lists(line2, "[[2]]") < 0;
+		dec2 += compare_lists(line1, "[[6]]") < 0;
+		dec2 += compare_lists(line2, "[[6]]") < 0;
 
-		fgets(buf1, sizeof(buf1), stdin);
+		fgets(line1, sizeof(line1), stdin);
 	}
 
 	printf("13: %d %d\n", p1,
