7f5031eecce61755ee2263c76cf428ff04bb4f51
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-14 01:23:36 +0100

    Makefile and run script for Turbo C 2.01

---
+++ 2022/c/all.bat
@@ -0,0 +1,13 @@
+day01.exe ..\data\01-input.txt
+day02.exe ..\data\02-input.txt
+day03.exe ..\data\03-input.txt
+day04.exe ..\data\04-input.txt
+day05.exe ..\data\05-input.txt
+day06.exe ..\data\06-input.txt
+REM day07.exe ..\data\07-input.txt
+day08.exe ..\data\08-input.txt
+REM day09.exe ..\data\09-input.txt
+day10.exe ..\data\10-input.txt
+REM day11.exe ..\data\11-input.txt
+day12.exe ..\data\12-input.txt
+day13.exe ..\data\13-input.txt
