3267f6c1223c759946c3e7c0ae16e87bb16c29a4
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-14 13:06:58 +0100

    Input stats script

---
+++ 2022/input-stats.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "lines:     " $(wc -l $1 | awk '{print $1}')
+echo "max length:" $(wc -L $1 | awk '{print $1}')
+
+grep -Eo '[0-9]+' $1 | awk '
+      { if ($0 < min || !have_min) { min = $0; have_min = 1; }
+        if ($0 > max || !have_max) { max = $0; have_max = 1; } }
+  END { print("min int:   ", min);
+	print("max int:   ", max); }'
