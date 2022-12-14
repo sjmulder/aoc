9d753809198367fa08b0193b725bcc71dbf06c44
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-14 13:06:46 +0100

    Visualisation helpers

---
+++ 2022/c/vis.h
@@ -0,0 +1,28 @@
+struct vis {
+	char *frame;
+	size_t w, h;
+
+	pid_t ffmpeg_pid;
+	FILE *ffmpeg_file;
+};
+
+struct vis_grid {
+	uint8_t *data;
+	uint8_t colors[256][3];
+	size_t w, h;
+	size_t cell_sz, border;
+};
+
+void vis_begin(
+    struct vis *,
+    char *filename,
+    size_t fps, size_t w, size_t h);
+
+void vis_fill(
+    struct vis *,
+    size_t x0, size_t y0, size_t w, size_t h,
+    uint8_t r, uint8_t g, uint8_t b);
+
+void vis_draw_grid(struct vis *, struct vis_grid *);
+void vis_emit(struct vis *, size_t duration);
+void vis_end(struct vis *);
