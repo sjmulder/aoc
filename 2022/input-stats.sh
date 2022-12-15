7b8bcbf54b53331570bed2745e0636dc18831e0b
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-16 00:43:32 +0100

    Support negative numbers in input-stats.sh

--- 2022/input-stats.sh
+++ 2022/input-stats.sh
@@ -3,7 +3,7 @@
 echo "lines:     " $(wc -l $1 | awk '{print $1}')
 echo "max length:" $(wc -L $1 | awk '{print $1}')
 
-grep -Eo '[0-9]+' $1 | awk '
+grep -Eo -- '-?[0-9]+' $1 | awk '
       { if ($0 < min || !have_min) { min = $0; have_min = 1; }
         if ($0 > max || !have_max) { max = $0; have_max = 1; } }
   END { print("min int:   ", min);
