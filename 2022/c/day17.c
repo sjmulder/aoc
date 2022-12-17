f48a26db32c6b3d35be4efe8b22f45bee55a5e1c
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-17 21:26:05 +0100

    2022 day 17, part 2 w/ cycle detection

--- 2022/c/day17.c
+++ 2022/c/day17.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <ctype.h>
+#include <inttypes.h>
 #include <assert.h>
 #include "compat/util.h"
 
@@ -10,9 +11,11 @@
 #define SW	4
 #define SH	4
 #define GW	7
-#define GH	4096
+#define GH	8192
 #define SPAWNY	3	/* offset from top */
 #define P2	1000000000000
+
+struct hist { int t, pi, top; int64_t nstack; };
 
 static const char shapes[][SH][SW+1] = {
 	{ "....", "....", "....", "####" },
@@ -24,8 +27,11 @@ static const char shapes[][SH][SW+1] = {
 
 
 static char grid[GH][GW];
-static int ymin=GH;			/* ymin: stack top */
+static struct hist hist[4096];
+static size_t nhist;
+static int t, top=GH;			/* top: stack top */
 static int pi, px=2, py=GH-SH-SPAWNY;	/* piece index, pos */
+static int64_t nstack;		/* no. stacked */
 
 static int min(int a, int b) { return a<b ? a : b; }
 //static int max(int a, int b) { return a>b ? a : b; }
@@ -35,9 +41,9 @@ print_grid(void)
 {
 	int x,y;
 
-	printf(" top: %d, piece %d at %d,%d\n", GH-ymin, pi, px,py);
+	printf(" top: %d, piece %d at %d,%d\n", GH-top, pi, px,py);
 
-	for (y = py-1; y < min(GH, ymin+10); y++) {
+	for (y = py-1; y < min(GH, top+10); y++) {
 		putchar('|');
 
 		for (x=0; x<GW; x++) {
@@ -58,6 +64,47 @@ print_grid(void)
 	for (x=0; x<GW; x++)
 		putchar('-');
 	printf("+\n\n");
+}
+
+static void
+hist_push(void)
+{
+	assert(nhist < LEN(hist));
+	hist[nhist].t = t;
+	hist[nhist].pi = pi;
+	hist[nhist].top = top;
+	hist[nhist].nstack = nstack;
+	nhist++;
+}
+
+/* reverse search first 'lim' of history for t+pi match */
+static struct hist *
+hist_find(size_t lim)
+{
+	size_t i;
+
+	for (i=0; i<lim; i++)
+		if (hist[lim-i-1].t == t && hist[lim-i-1].pi == pi)
+			return &hist[lim-i-1];
+
+	return NULL;
+}
+
+static int
+find_cycle(int *dtop, int64_t *dnstack)
+{
+	struct hist *h1=hist+nhist, *h2;
+
+	while ((h1 = h2 = hist_find(h1-hist)))
+	while ((h2 = hist_find(h2-hist)))
+		if (h1->top - top == h2->top - h1->top &&
+		    nstack - h1->nstack == h1->nstack - h2->nstack) {
+			*dtop = h1->top - top;
+			*dnstack = nstack - h1->nstack;
+			return 1;
+		}
+
+	return 0;
 }
 
 static int
@@ -84,15 +131,16 @@ hit_test(int dx, int dy)
 int
 main() {
 	static char jets[16*1024];
-	int nr, t=0, dx, x,y;
-	int64_t nsettled=0;
+	int nr, dx, x,y;
+	int dtop;
+	int64_t dnstack, yskip=0, ncycles;
 
 	nr = (int)fread(jets, 1, sizeof(jets), stdin);
 	while (nr>0 && isspace(jets[nr-1]))
 		nr--;
 
-	while (nsettled < 1000000000000) {
-		switch (jets[t++ % nr]) {
+	for (t=0; nstack < P2; t = (t+1) % nr) {
+		switch (jets[t]) {
 		case '<': dx=-1; break;
 		case '>': dx= 1; break;
 		default: goto end;
@@ -113,20 +161,30 @@ main() {
 			    py+y >= 0 && py+y < GH &&
 			    shapes[pi][y][x] == '#') {
 				grid[py+y][px+x] = 1;
-				ymin = min(ymin, py+y);
+				top = min(top, py+y);
 			}
 
-		if (++nsettled == 2022) {
-			printf("17: %d ", GH-ymin);
+		if (++nstack == 2022) {
+			printf("17: %d ", GH-top);
 			fflush(stdout);
+		}
+
+		if (!yskip) {
+			hist_push();
+
+			if (find_cycle(&dtop, &dnstack)) {
+				ncycles = (P2-nstack)/dnstack;
+				yskip   = ncycles * dtop;
+				nstack += ncycles * dnstack;
+			}
 		}
 
 		pi = (pi+1) % (int)LEN(shapes);
 		px = 2;
-		py = ymin-SH-SPAWNY;
+		py = top-SH-SPAWNY;
 		assert(py >= 0);
 	}
 end:
-	printf("%d\n", GH-ymin);
+	printf("%" PRId64 "\n", yskip + GH-top);
 	return 0;
 }
