b6c175eed815bfcba1e119e13723219caaf5da7b
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-20 18:01:03 +0100

    Drop 'ix' table and inline wrap()
    It is faster do do a search now and then than it is to keep ix updated.

--- 2022/c/day20.c
+++ 2022/c/day20.c
@@ -7,61 +7,51 @@
 
 #define SZ	5120
 
-static int wrap(int i, int n) { return (i %n +n) %n; }
-
 /*
  * id:  the 'identity' of the number (its original index)
  * idx: index in the output array
  */
 static int64_t val[SZ];	/* values by id */
-static int ix[SZ];	/* indexes by id */
 static int id[SZ];	/* id by index */
 static int n, zero_id;
 
 static int64_t
 run(int steps)
 {
-	int s,i,j, f,t;
+	int s,i, f,t;
 	int64_t v;
 
-	for (i=0; i<n; i++) ix[i] = i;
 	for (i=0; i<n; i++) id[i] = i;
 
 	for (s=0; s<steps; s++)
 	for (i=0; i<n; i++) {
 		v = val[i];
-		f = ix[i];	/* f: from index, t: to index */
-		t = wrap(f + (int)(v%(n-1)), n);
+		for (f=0; id[f]!=i; f++) ;	/* from idx */
+		t = (((f + v%(n-1)) %n) +n) %n;	/* to idx */
 
-		if (v>0 && t>f) {
+		if (v>0 && t>f)
 			memmove(id+f, id+f+1, (t-f)*sizeof(*id));
-			id[t] = i;
-			for (j=f; j<=t; j++) ix[id[j]] = j;
-		} else if (v<0 && t<f) {
+		else if (v<0 && t<f)
 			memmove(id+t+1, id+t, (f-t)*sizeof(*id));
-			id[t] = i;
-			for (j=t; j<=f; j++) ix[id[j]] = j;
-		} else if (v>0 && t<f) {
+		else if (v>0 && t<f) {
 			memmove(id+f, id+f+1, (n-f-1)*sizeof(*id));
 			id[n-1] = id[0];
 			memmove(id, id+1, t*sizeof(*id));
-			id[t] = i;
-			for (j=0; j<=t; j++) ix[id[j]] = j;
-			for (j=f; j<n;  j++) ix[id[j]] = j;
 		} else if (v<0 && t>f) {
 			memmove(id+1, id, f*sizeof(*id));
 			id[0] = id[n-1];
 			memmove(id+t+1, id+t, (n-t-1)*sizeof(*id));
-			id[t] = i;
-			for (j=0; j<=f; j++) ix[id[j]] = j;
-			for (j=t; j<n;  j++) ix[id[j]] = j;
 		}
+
+		id[t] = i;
 	}
 
+	for (i=0; id[i] != zero_id; i++) ;
+
 	return
-	    val[id[wrap(ix[zero_id] + 1000, n)]] +
-	    val[id[wrap(ix[zero_id] + 2000, n)]] +
-	    val[id[wrap(ix[zero_id] + 3000, n)]];
+	    val[id[(i+1000)%n]] +
+	    val[id[(i+2000)%n]] +
+	    val[id[(i+3000)%n]];
 }
 
 int
