a41b29c06288578dec4d6add7fb35b20782d58a4
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-23 00:24:59 +0100

    Extract common.mk from 2022

---
+++ mk/common.mk
@@ -0,0 +1,21 @@
+# Implements the following targets:
+#
+#   run   [DAYS=..] [INPUT=..]
+#   bench [DAYS=..] [INPUT=..] [BENCH=..]
+#
+# Both are run with input files derrived from the program names, this
+# can be overridden with INPUT or INPUT.<name> (e.g. INPUT.day12).
+
+BENCH?=		time -f "$p  %E  %M Kb  %F+%R faults"
+
+run:   ${DAYS} ; @${RUN_CMD}
+bench: ${DAYS} ; @${BENCH_CMD}
+
+.for p in ${DAYS}
+INPUT.$p?=	${INPUT:U../../inputs/2022/${p:S/day//}-input.txt}
+RUN_CMD+=	./$p ${INPUT.$p};
+BENCH_CMD+=	${BENCH} ./$p <${INPUT.$p} >/dev/null;
+.endfor
+
+.o:
+	${LINK.c} -o $@ $> ${LDLIBS}
