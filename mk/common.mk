3d4fed1d22be91aa046b507cfccb44ab0e29dccb
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-23 07:46:19 +0100

    Q(uiet) var for run/bench targets

--- mk/common.mk
+++ mk/common.mk
@@ -6,10 +6,11 @@
 # Both are run with input files derrived from the program names, this
 # can be overridden with INPUT or INPUT.<name> (e.g. INPUT.day12).
 
+Q?=		@
 BENCH?=		time -f "$p  %E  %M Kb  %F+%R faults"
 
-run:   ${DAYS} ; @${RUN_CMD}
-bench: ${DAYS} ; @${BENCH_CMD}
+run:   ${DAYS} ; $Q${RUN_CMD}
+bench: ${DAYS} ; $Q${BENCH_CMD}
 
 .for p in ${DAYS}
 INPUT.$p?=	${INPUT:U../../inputs/2022/${p:S/day//}-input.txt}
