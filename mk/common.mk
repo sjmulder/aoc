685903c22d2c9c1ee382a09869c035d063a14c9e
Sijmen J. Mulder <ik@sjmulder.nl>
2022-12-26 00:58:02 +0100

    Rework bench target for macOS

--- mk/common.mk
+++ mk/common.mk
@@ -1,4 +1,4 @@
-# Implements the following targets:
+# Implements some common things including run and bench targetsL
 #
 #   run   [DAYS=..] [INPUT=..]
 #   bench [DAYS=..] [INPUT=..] [BENCH=..]
@@ -6,17 +6,28 @@
 # Both are run with input files derrived from the program names, this
 # can be overridden with INPUT or INPUT.<name> (e.g. INPUT.day12).
 
-Q?=		@
-BENCH?=		time -f "$p  %E  %M Kb  %F+%R faults"
+UNAME!=		uname
 
-run:   ${DAYS} ; $Q${RUN_CMD}
-bench: ${DAYS} ; $Q${BENCH_CMD}
+.if "${UNAME}" == "Darwin"
+TIME?=		time
+SHELL=		zsh
+.endif
+
+Q?=		@
+TIME?=		time -f ${TIMEFMT}
+TIMEFMT?=	" %E  %M Kb  %F+%R faults"
+
+ENV+=		TIMEFMT=${TIMEFMT}
 
 .for p in ${DAYS}
 INPUT.$p?=	${INPUT:U../../inputs/2022/${p:S/day//}-input.txt}
+
 RUN_CMD+=	./$p <${INPUT.$p};
-BENCH_CMD+=	${BENCH} ./$p <${INPUT.$p} >/dev/null;
+BENCH_CMD+=	printf "$p "; ${TIME} ./$p <${INPUT.$p} >/dev/null;
 .endfor
+
+run:   ${DAYS} ; $Q env ${ENV} ${SHELL} -c ${RUN_CMD:Q}
+bench: ${DAYS} ; $Q env ${ENV} ${SHELL} -c ${BENCH_CMD:Q}
 
 .o:
 	${LINK.c} -o $@ $> ${LDLIBS}
