4d52da5bf1a5f450b91b477fb165d6de73dfe872
Sijmen J. Mulder <ik@sjmulder.nl>
2023-12-02 11:32:55 +0100

    Golfed

---
+++ 2023/c-golf/day02-narrow.c
@@ -0,0 +1,7 @@
+c,p,P,i,n,r,g,b;main(){while(~
+(c=getchar()))c==44|c==58|59==
+c?n=0:c>47&c<58?n=n*10+c-48:98
+==c?b=b>n?b:n:c=='d'?r=r>n?r:n
+:c=='g'?g=g>n?g:n:10==c?p+=++i
+*(r<13&g<14&b<15),P+=r*g*b,r=g
+=b=0:0;printf("%d %d\n",p,P);}
---
+++ 2023/c-golf/day02.c
@@ -0,0 +1,3 @@
+c,p,P,i,n,r,g,b;main(){while(~(c=getchar()))c==44|c==58|c==59?n=0:c>47
+&c<58?n=n*10+c-48:c=='d'?r=r>n?r:n:c=='g'?g=g>n?g:n:c==98?b=b>n?b:n:10
+==c?p+=++i*(r<13&g<14&b<15),P+=r*g*b,r=g=b=0:0;printf("%d %d\n",p,P);}
