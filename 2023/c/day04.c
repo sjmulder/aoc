8b55ee2965a4dc2ef7e79cb74dbcd27158b82cde
Sijmen J. Mulder <ik@sjmulder.nl>
2023-12-04 10:27:20 +0100

    Drop asserts for brevity

--- 2023/c/day04.c
+++ 2023/c/day04.c
@@ -1,44 +1,33 @@
 #include "common.h"
 
-#define MAXNUMS		10
-#define MAXCARDS	200
-
 int
 main(int argc, char **argv)
 {
 	char line[128], *rest, *tok;
-	int nums[MAXNUMS], nnums;
-	int ncopy[MAXCARDS] = {0};
-	int p1=0,p2=0, id, val, nmatch, i;
+	int nextra[200]={0}, nums[10], nnums;
+	int p1=0,p2=0, id,val,nmatch, i;
 
 	if (argc > 1)
 		DISCARD(freopen(argv[1], "r", stdin));
 	
 	for (id=0; (rest = fgets(line, sizeof(line), stdin)); id++) {
-		assert(id < MAXCARDS);
 		nnums = nmatch = 0;
 
 		while ((tok = strsep(&rest, " ")) && !strchr(tok, ':'))
 			;
-
 		while ((tok = strsep(&rest, " ")) && !strchr(tok, '|'))
-			if ((val = atoi(tok))) {
-				assert(nnums < MAXNUMS);
+			if ((val = atoi(tok)))
 				nums[nnums++] = val;
-			}
-
 		while ((tok = strsep(&rest, " ")))
 			if ((val = atoi(tok)))
 				for (i=0; i<nnums; i++)
 					nmatch += nums[i] == val;
 
-		assert(id + nmatch <= MAXCARDS);
-
 		for (i=0; i<nmatch; i++)
-			ncopy[id+1+i] += ncopy[id]+1;
+			nextra[id+1+i] += nextra[id]+1;
 
 		p1 += nmatch ? 1 << (nmatch-1) : 0;
-		p2 += ncopy[id]+1;
+		p2 += nextra[id]+1;
 	}
 
 	printf("%d %d\n", p1, p2);
