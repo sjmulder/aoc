5229cfad6bf9dc95cd085415bb868e74abee5798
Sijmen J. Mulder <ik@sjmulder.nl>
2023-12-04 10:25:35 +0100

    Process win numbers inline

--- 2023/c/day04.c
+++ 2023/c/day04.c
@@ -1,7 +1,6 @@
 #include "common.h"
 
 #define MAXNUMS		10
-#define MAXWINS		25
 #define MAXCARDS	200
 
 int
@@ -9,42 +8,29 @@ main(int argc, char **argv)
 {
 	char line[128], *rest, *tok;
 	int nums[MAXNUMS], nnums;
-	int wins[MAXWINS], nwins;
 	int ncopy[MAXCARDS] = {0};
-	int p1=0,p2=0, id, nmatch, i,j;
+	int p1=0,p2=0, id, val, nmatch, i;
 
 	if (argc > 1)
 		DISCARD(freopen(argv[1], "r", stdin));
 	
 	for (id=0; (rest = fgets(line, sizeof(line), stdin)); id++) {
 		assert(id < MAXCARDS);
-		nnums = nwins = nmatch = 0;
+		nnums = nmatch = 0;
 
-		while ((tok = strsep(&rest, " ")))
-			if (strchr(tok, ':'))
-				break;
+		while ((tok = strsep(&rest, " ")) && !strchr(tok, ':'))
+			;
 
-		while ((tok = strsep(&rest, " "))) {
-			if (strchr(tok, '|'))
-				break;
-			if (*tok) {
+		while ((tok = strsep(&rest, " ")) && !strchr(tok, '|'))
+			if ((val = atoi(tok))) {
 				assert(nnums < MAXNUMS);
-				nums[nnums++] = atoi(tok);
+				nums[nnums++] = val;
 			}
-		}
 
 		while ((tok = strsep(&rest, " ")))
-			if (*tok) {
-				assert(nwins < MAXWINS);
-				wins[nwins++] = atoi(tok);
-			}
-
-		for (i=0; i<nnums; i++)
-		for (j=0; j<nwins; j++)
-			if (nums[i] == wins[j]) {
-				nmatch++;
-				break;
-			}
+			if ((val = atoi(tok)))
+				for (i=0; i<nnums; i++)
+					nmatch += nums[i] == val;
 
 		assert(id + nmatch <= MAXCARDS);
 
