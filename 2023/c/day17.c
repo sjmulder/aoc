8c4ec883b26ac5f5d9a36fea15b27194bed35357
Sijmen J. Mulder <ik@sjmulder.nl>
2023-12-17 21:33:02 +0100

    Prefill costs[] with INT_MAX to simplify lookup
    Also starting steps from 0, forgot to commit that separately.

--- 2023/c/day17.c
+++ 2023/c/day17.c
@@ -3,14 +3,14 @@
 #define GW 148
 #define GH 148
 #define ND 4
-#define NS 11
+#define NS 10
 
 enum { NN, EE, SS, WW };
 
 struct node { int x,y,d,s; };
 
 static char map[GH][GW];
-static int costs[GH][GW][ND][NS];	/* y,x,dist,ss */
+static int costs[GH][GW][ND][NS];	/* y,x,dist,steps */
 static int ages[GH][GW][ND][NS];
 static int w,h, cur_age;
 
@@ -21,8 +21,8 @@ is_valid(struct node a, struct node b, int smin, int smax)
 	    b.x >= 0 && b.x < w &&
 	    b.y >= 0 && b.y < h &&
 	    abs(b.d-a.d) != 2 &&
-	    (a.s >= smin || a.d == b.d) &&
-	     b.s <= smax;
+	    (a.s+1 >= smin || a.d == b.d) &&
+	     b.s+1 <= smax;
 }
 
 static struct node
@@ -33,51 +33,41 @@ node_from(struct node a, int d)
 	b.x = d==EE ? a.x+1 : d==WW ? a.x-1 : a.x;
 	b.y = d==SS ? a.y+1 : d==NN ? a.y-1 : a.y;
 	b.d = d;
-	b.s = d==a.d ? a.s+1 : 1;
+	b.s = d==a.d ? a.s+1 : 0;
 
 	return b;
-}
-
-static int
-get_cost(struct node n)
-{
-	int cost;
-
-	cost = costs[n.y][n.x][n.d][n.s];
-
-	return cost || (n.x==0 && n.y==0 && n.d==EE && n.s==0)
-	    ? cost : INT_MAX;
-}
-
-static void
-set_cost(struct node n, int cost)
-{
-	costs[n.y][n.x][n.d][n.s] = cost;
 }
 
 static int
 step_flood(int smin, int smax)
 {
 	struct node a, b;
-	int nchange=0, a_cost, b_cost, d;
+	int nchange=0, a_cost,b_cost,b_cur, d;
 
 	for (a.y=0; a.y<h; a.y++)
 	for (a.x=0; a.x<w; a.x++)
 	for (a.d=0; a.d<ND; a.d++)
-	for (a.s=0; a.s <= smax; a.s++)
-		if (ages[a.y][a.x][a.d][a.s] >= cur_age-1)
-		if ((a_cost = get_cost(a)) != INT_MAX)
-			for (d=0; d<ND; d++) {
-				b = node_from(a, d);
-				b_cost = a_cost + map[b.y][b.x]-'0';
+	for (a.s=0; a.s<smax; a.s++) {
+		if (ages[a.y][a.x][a.d][a.s] < cur_age-1)
+			continue;
+		if ((a_cost = costs[a.y][a.x][a.d][a.s]) == INT_MAX)
+			continue;
+		for (d=0; d<ND; d++) {
+			b = node_from(a, d);
 
-				if (is_valid(a, b, smin, smax))
-				if (b_cost < get_cost(b)) {
-					set_cost(b, b_cost);
-					ages[b.y][b.x][b.d][b.s] = cur_age;
-					nchange++;
-				}
+			if (!is_valid(a, b, smin, smax))
+				continue;
+
+			b_cost = a_cost + map[b.y][b.x]-'0';
+			b_cur = costs[b.y][b.x][b.d][b.s];
+
+			if (b_cost < b_cur) {
+				costs[b.y][b.x][b.d][b.s] = b_cost;
+				ages[b.y][b.x][b.d][b.s] = cur_age;
+				nchange++;
 			}
+		}
+	}
 
 	cur_age++;
 
@@ -87,20 +77,27 @@ step_flood(int smin, int smax)
 static int
 solve(int smin, int smax)
 {
-	int best=INT_MAX, d,st;
+	int best=INT_MAX, x,y,d,s;
 
-	memset(costs, 0, sizeof(costs));
-	memset(ages, 0, sizeof(ages));
+	for (y=0; y<h; y++)
+	for (x=0; x<w; x++)
+	for (d=0; d<ND; d++)
+	for (s=0; s<smax; s++) {
+		costs[y][x][d][s] = INT_MAX;
+		ages[y][x][d][s] = 0;
+	}
+
 	cur_age = 0;
+	costs[0][1][EE][0] = 0;
 
 	while (step_flood(smin, smax))
 		;
 
 	for (d=0; d<ND; d++)
-	for (st=smin; st<smax; st++)
-		if (costs[h-1][w-1][d][st] &&
-		    costs[h-1][w-1][d][st] < best)
-			best = costs[h-1][w-1][d][st];
+	for (s=smin; s<smax; s++)
+		if (costs[h-1][w-1][d][s] &&
+		    costs[h-1][w-1][d][s] < best)
+			best = costs[h-1][w-1][d][s];
 
 	return best;
 }
