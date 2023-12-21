48b1ed882d630ee18636ff072e4f41f44df15d77
Sijmen J. Mulder <ik@sjmulder.nl>
2023-12-21 09:16:05 +0100

    2023 day 21, part 1

---
+++ 2023/c/day21.c
@@ -0,0 +1,46 @@
+#include "common.h"
+
+#define GSZ 135
+#define TSZ 65	/* time dimension */
+
+static char map[GSZ][GSZ];
+static uint8_t reach[GSZ][GSZ][TSZ];
+
+int
+main(int argc, char **argv)
+{
+	int p1=0, w,h, x,y,t;
+
+	if (argc > 1)
+		DISCARD(freopen(argv[1], "r", stdin));
+
+	for (y=1; fgets(map[y]+1, sizeof(map[y])-2, stdin); y++) {
+		printf("char at end: 0x%02x. string: %s\n",
+		    map[y][strlen(map[y]+1)],
+		    map[y]+1);
+		assert(map[y][strlen(map[y]+1)] == '\n');
+		assert(y+1 < GSZ-1);
+	}
+
+	for (y=1; y<GSZ-1; y++)
+	for (x=1; x<GSZ-1; x++)
+		if (map[y][x] == 'S') {
+			reach[y][x][0] = 1;
+			break;
+		}
+
+	for (t=1; t<=64; t++)
+	for (y=1; y<GSZ-1; y++)
+	for (x=1; x<GSZ-1; x++)
+		reach[y][x][t] =
+		    (map[y][x] == '.' || map[y][x] == 'S') &&
+		    (reach[y][x-1][t-1] || reach[y-1][x][t-1] ||
+		     reach[y][x+1][t-1] || reach[y+1][x][t-1]);
+
+	for (y=1; y<GSZ-1; y++)
+	for (x=1; x<GSZ-1; x++)
+		p1 += reach[y][x][t-1];
+
+	printf("21: %d\n", p1);
+	return 0;
+}
