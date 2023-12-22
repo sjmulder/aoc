e715234cc575192e7566d914f1cacb249b0fbe10
Sijmen J. Mulder <ik@sjmulder.nl>
2023-12-22 09:17:06 +0100

    Drop the indexing safety ring

--- 2023/c/day21.c
+++ 2023/c/day21.c
@@ -13,28 +13,30 @@ main(int argc, char **argv)
 	if (argc > 1)
 		DISCARD(freopen(argv[1], "r", stdin));
 
-	for (y=1; fgets(map[y]+1, sizeof(map[y])-2, stdin); y++) {
+	for (y=1; fgets(map[y], sizeof(map[y]), stdin); y++) {
 		assert(map[y][strlen(map[y]+1)] == '\n');
 		assert(y+1 < GSZ-1);
 	}
 
-	for (y=1; y<GSZ-1; y++)
-	for (x=1; x<GSZ-1; x++)
+	for (y=0; y<GSZ; y++)
+	for (x=0; x<GSZ; x++)
 		if (map[y][x] == 'S') {
 			reach[y][x][0] = 1;
 			break;
 		}
 
 	for (t=0; t<64; t++)
-	for (y=1; y<GSZ-1; y++)
-	for (x=1; x<GSZ-1; x++)
+	for (y=0; y<GSZ; y++)
+	for (x=0; x<GSZ; x++)
 		reach[y][x][(t+1)&1] =
 		    (map[y][x] == '.' || map[y][x] == 'S') &&
-		    (reach[y][x-1][t&1] || reach[y-1][x][t&1] ||
-		     reach[y][x+1][t&1] || reach[y+1][x][t&1]);
+		    ((x>=0    && reach[y][x-1][t&1]) ||
+		     (x<GSZ-1 && reach[y][x+1][t&1]) ||
+		     (y>=0    && reach[y-1][x][t&1]) ||
+		     (y<GSZ-1 && reach[y+1][x][t&1]));
 
-	for (y=1; y<GSZ-1; y++)
-	for (x=1; x<GSZ-1; x++)
+	for (y=0; y<GSZ; y++)
+	for (x=0; x<GSZ; x++)
 		p1 += reach[y][x][t&1];
 
 	printf("21: %d <TODO>\n", p1);
