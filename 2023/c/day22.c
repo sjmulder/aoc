144077db8125bb685cde2ee08a28dadfab473b54
Sijmen J. Mulder <ik@sjmulder.nl>
2023-12-23 01:01:11 +0100

    2023 day 22, part 2

--- 2023/c/day22.c
+++ 2023/c/day22.c
@@ -6,36 +6,48 @@
 
 struct block { int x1,y1,z1, x2,y2,z2; };
 
-static struct block blocks[1600];
+static struct block bricks[1600];
 static struct block *grid[ZSZ][YSZ][XSZ];
-static int nblocks;
+static size_t nbricks;
+
+static struct block
+above(struct block b)
+{
+	b.z1 = b.z2 = b.z2+1;
+	return b;
+}
+
+static struct block
+below(struct block b)
+{
+	b.z1 = b.z2 = b.z1-1;
+	return b;
+}
 
 static void
-paint(struct block *b, struct block *val)
+paint(struct block b, struct block *val)
 {
 	int x,y,z;
 
-	for (z=b->z1; z<=b->z2; z++)
-	for (y=b->y1; y<=b->y2; y++)
-	for (x=b->x1; x<=b->x2; x++)
+	for (z=b.z1; z<=b.z2; z++)
+	for (y=b.y1; y<=b.y2; y++)
+	for (x=b.x1; x<=b.x2; x++)
 		grid[z][y][x] = val;
 }
 
-static int
-scan(struct block *b, struct block **hits, int sz)
+static size_t
+scan(struct block b, struct block **hits, size_t sz)
 {
 	static struct block *buf[5];
 	struct block *hit;
-	int nhits=0, x,y,z, i;
+	size_t nhits=0, i;
+	int x,y,z;
 
-	if (!hits) {
-		hits = buf;
-		sz = (int)LEN(buf);
-	}
+	if (!hits) { hits = buf; sz = LEN(buf); }
 
-	for (z=b->z1; z<=b->z2; z++)
-	for (y=b->y1; y<=b->y2; y++)
-	for (x=b->x1; x<=b->x2; x++) {
+	for (z=b.z1; z<=b.z2; z++)
+	for (y=b.y1; y<=b.y2; y++)
+	for (x=b.x1; x<=b.x2; x++) {
 		if (!(hit = grid[z][y][x]))
 			continue;
 		for (i=0; i<nhits && hit != hits[i]; i++)
@@ -52,51 +64,78 @@ scan(struct block *b, struct block **hits, int sz)
 static void
 gravitas(void)
 {
-	struct block below;
-	int dirty, i;
+	size_t i;
+	int dirty;
 
 	for (dirty=1; dirty; )
-	for (dirty=0, i=0; i<nblocks; i++) {
-		if (!blocks[i].z1)
-			continue;
-
-		below = blocks[i];
-		below.z1 = below.z2 = below.z1-1;
-
-		if (!scan(&below, NULL, 0)) {
-			paint(&blocks[i], NULL);
-			blocks[i].z1--;
-			blocks[i].z2--;
-			paint(&blocks[i], &blocks[i]);
+	for (dirty=0, i=0; i<nbricks; i++)
+		if (bricks[i].z1 && !scan(below(bricks[i]), NULL, 0)) {
+			paint(below(bricks[i]), &bricks[i]);
+			bricks[i].z1--;
+			bricks[i].z2--;
+			paint(above(bricks[i]), NULL);
 			dirty = 1;
 		}
-	}
 }
 
 static int
 jenga(void)
 {
 	static struct block *hits[5];
-	struct block above, below;
-	int count=0, deps, nabove, i,j;
+	int count=0;
+	size_t nabove, i,j;
 
-	for (i=0; i<nblocks; i++) {
-		above = blocks[i];
-		above.z1 = above.z2 = above.z2+1;
-		nabove = scan(&above, hits, (int)LEN(hits));
-		deps = 0;
+	for (i=0; i<nbricks; i++) {
+		nabove = scan(above(bricks[i]), hits, (int)LEN(hits));
+		for (j=0; j<nabove; j++)
+			if (scan(below(*hits[j]), NULL, 0) < 2)
+				break;
 
-		for (j=0; j<nabove; j++) {
-			below = *hits[j];
-			below.z1 = below.z2 = below.z1-1;
-			deps += scan(&below, NULL, 0) < 2;
-		}
-
-		if (!deps)
-			count++;
+		count += j == nabove;
 	}
 
 	return count;
+}
+
+static void
+disintegrate(struct block *b, uint8_t *gone)
+{
+	struct block *up[5], *down[5];
+	size_t nup, ndown, i,j;
+
+	if (gone[b-bricks])
+		return;
+	
+	gone[b-bricks] = 1;
+
+	nup = scan(above(*b), up, LEN(up));
+	for (i=0; i<nup; i++) {
+		ndown = scan(below(*up[i]), down, LEN(down));
+		for (j=0; j<ndown; j++)
+			if (!gone[down[j]-bricks])
+				break;
+
+		if (j == ndown)
+			disintegrate(up[i], gone);
+	}
+}
+
+static uint64_t
+godzilla(void)
+{
+	static uint8_t gone[LEN(bricks)];
+	size_t i,j;
+	uint64_t ngone=0;
+
+	for (i=0; i<nbricks; i++) {
+		memset(gone, 0, sizeof(gone));
+		disintegrate(&bricks[i], gone);
+
+		for (j=0; j<nbricks; j++)
+			ngone += gone[j];
+	}
+
+	return ngone - nbricks;	/* don't count fist bricks */
 }
 
 int
@@ -104,13 +143,14 @@ main(int argc, char **argv)
 {
 	struct block *b;
 	int p1, ntok;
+	uint64_t p2;
 
 	if (argc > 1)
 		DISCARD(freopen(argv[1], "r", stdin));
 	
-	for (; ; nblocks++) {
-		assert(nblocks < (int)LEN(blocks));
-		b = &blocks[nblocks];
+	for (; ; nbricks++) {
+		assert(nbricks < LEN(bricks));
+		b = &bricks[nbricks];
 		ntok = scanf("%d,%d,%d~%d,%d,%d",
 		    &b->x1, &b->y1, &b->z1,
 		    &b->x2, &b->y2, &b->z2);
@@ -128,12 +168,13 @@ main(int argc, char **argv)
 		assert(b->z1 >= 0); assert(b->z1 < ZSZ);
 		assert(b->z2 >= 0); assert(b->z2 < ZSZ);
 
-		paint(b, b);
+		paint(*b, b);
 	}
 
 	gravitas();
 	p1 = jenga();
+	p2 = godzilla();
 
-	printf("22: %d <TODO>\n", p1);
+	printf("22: %d %"PRIu64"\n", p1, p2);
 	return 0;
 }
