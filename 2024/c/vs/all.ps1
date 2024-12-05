d0d941844bd700bd0fecc021231c82fcf8dc6c25
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-05 13:43:12 +0100

    Visual C++ run script for 2024

---
+++ 2024/c/vs/all.ps1
@@ -0,0 +1,8 @@
+# Usage: all.ps1 <CONFIGURATION>
+# e.g.:  all.ps1 "x64 Release"
+
+& "./bin/$args/day01.exe" ../../../inputs/2024/01-input.txt
+& "./bin/$args/day02.exe" ../../../inputs/2024/02-input.txt
+& "./bin/$args/day03.exe" ../../../inputs/2024/03-input.txt
+& "./bin/$args/day04.exe" ../../../inputs/2024/04-input.txt
+& "./bin/$args/day05.exe" ../../../inputs/2024/05-input.txt
