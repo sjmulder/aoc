2d9b19b07b557401222f50ed7043d1caee2252dd
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-05 02:17:56 +0100

    Early break if no match

--- 2024/c/day04.c
+++ 2024/c/day04.c
@@ -7,21 +7,21 @@ main(int argc, char **argv)
 {
 	static char g[GZ][GZ];
 	static const char w[] = "XMAS";
-	int p1=0,p2=0, x,y, m,i;
+	int p1=0,p2=0, x,y, i;
 
 	if (argc > 1) DISCARD(freopen(argv[1], "r", stdin));
 	for (y=3; y<GZ && fgets(g[y]+3, GZ-3, stdin); y++) ;
 
 	for (y=3; y<GZ-3; y++)
 	for (x=3; x<GZ-3; x++) {
-		for (m=1,i=0; i<4; i++) {m &= g[y+i][x]==w[i];} p1+=m;
-		for (m=1,i=0; i<4; i++) {m &= g[y-i][x]==w[i];} p1+=m;
-		for (m=1,i=0; i<4; i++) {m &= g[y][x+i]==w[i];} p1+=m;
-		for (m=1,i=0; i<4; i++) {m &= g[y][x-i]==w[i];} p1+=m;
-		for (m=1,i=0; i<4; i++) {m &= g[y+i][x+i]==w[i];} p1+=m;
-		for (m=1,i=0; i<4; i++) {m &= g[y-i][x-i]==w[i];} p1+=m;
-		for (m=1,i=0; i<4; i++) {m &= g[y+i][x-i]==w[i];} p1+=m;
-		for (m=1,i=0; i<4; i++) {m &= g[y-i][x+i]==w[i];} p1+=m;
+		for (i=0; i<4 && g[y+i][x]==w[i]; i++) ; p1 += i==4;
+		for (i=0; i<4 && g[y-i][x]==w[i]; i++) ; p1 += i==4;
+		for (i=0; i<4 && g[y][x+i]==w[i]; i++) ; p1 += i==4;
+		for (i=0; i<4 && g[y][x-i]==w[i]; i++) ; p1 += i==4;
+		for (i=0; i<4 && g[y+i][x+i]==w[i]; i++) ; p1 += i==4;
+		for (i=0; i<4 && g[y-i][x-i]==w[i]; i++) ; p1 += i==4;
+		for (i=0; i<4 && g[y+i][x-i]==w[i]; i++) ; p1 += i==4;
+		for (i=0; i<4 && g[y-i][x+i]==w[i]; i++) ; p1 += i==4;
 
 		p2 += g[y+1][x+1]=='A' &&
 		      ((g[y][x]  =='M' && g[y+2][x+2]=='S')  ||
