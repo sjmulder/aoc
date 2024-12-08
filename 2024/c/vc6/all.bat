5319bf50b3f645fe0a095b756df71e135100f9c4
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-08 16:39:18 +0100

    VC++ 6 project for 2024 day 6

--- 2024/c/vc6/all.bat
+++ 2024/c/vc6/all.bat
@@ -3,3 +3,4 @@
 @Release\day03.exe ..\..\..\inputs\2024\03-input.txt
 @Release\day04.exe ..\..\..\inputs\2024\04-input.txt
 @Release\day05.exe ..\..\..\inputs\2024\05-input.txt
+@Release\day06.exe ..\..\..\inputs\2024\06-input.txt
