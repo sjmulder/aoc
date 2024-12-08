9cd1031f419b86cf37cb2a9c4465cf5e26769a75
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-08 16:42:09 +0100

    2024 day 8 in VC++ 6

---
+++ 2024/c/day08.c
@@ -0,0 +1,95 @@
+#include "common.h"
+
+#define GZ 52
+
+static char g[GZ][GZ];
+
+#define ANTI_P1 1
+#define ANTI_P2 2
+static uint8_t anti[GZ][GZ];
+
+static int w,h;
+
+static void
+dump(void)
+{
+	int x,y;
+
+	for (y=0; y<h; y++) {
+		for (x=0; x<w; x++)
+			putchar(
+			    isalnum(g[y][x]) ? g[y][x] : 
+			    (anti[y][x] & ANTI_P1) ? '#' :
+			    (anti[y][x] & ANTI_P2) ? '$' :
+			    isprint(g[y][x]) ? g[y][x] : '~');
+		putchar('\n');
+	}
+}
+
+int
+main(int argc, char **argv)
+{
+	int p1=0,p2=0, x,y, x1,y1, ax,ay, i;
+	char *lf;
+
+	if (argc > 1)
+		DISCARD(freopen(argv[1], "r", stdin));
+	for (h=0; h<GZ && fgets(g[h], GZ, stdin); h++)
+		;
+
+	assert(feof(stdin));
+	lf = strchr(g[0], '\n');
+	assert(lf);
+	w = lf - g[0];
+
+	//dump();
+	//putchar('\n');
+
+	/*
+	 * Find antenna pairs, then project backwards from the first,
+	 * forwards from the second. Don't like the repetition but it
+	 * makes for easy code.
+	 */
+	for (y=0; y<h; y++)
+	for (x=0; x<w; x++) {
+		if (!isalnum(g[y][x]))
+			continue;
+
+		for (y1=y; y1<h; y1++)
+		for (x1=(y==y1?x+1:0); x1<w; x1++) {
+			if (g[y][x] != g[y1][x1])
+				continue;
+
+			for (i=0; ; i++) {
+				if ((ax = x-(x1-x)*i) <0 || ax>w ||
+				    (ay = y-(y1-y)*i) <0 || ay>h)
+					break;
+				anti[ay][ax] |= ANTI_P1 * i==1;
+				anti[ay][ax] |= ANTI_P2;
+			}
+
+			for (i=0; ; i++) {
+				if ((ax = x1+(x1-x)*i) <0 || ax>w ||
+				    (ay = y1+(y1-y)*i) <0 || ay>h)
+					break;
+				anti[ay][ax] |= ANTI_P1 * i==1;
+				anti[ay][ax] |= ANTI_P2;
+			}
+
+			//dump();
+			//putchar('\n');
+		}
+	}
+
+	dump();
+	putchar('\n');
+
+	for (y=0; y<h; y++)
+	for (x=0; x<w; x++) {
+		p1 += !!(anti[y][x] & ANTI_P1);
+		p2 += !!(anti[y][x] & ANTI_P2);
+	}
+
+	printf("08: %d %d\n", p1, p2);
+	return 0;
+}
--- 2024/c/vc6/all.bat
+++ 2024/c/vc6/all.bat
@@ -4,3 +4,4 @@
 @Release\day04.exe ..\..\..\inputs\2024\04-input.txt
 @Release\day05.exe ..\..\..\inputs\2024\05-input.txt
 @Release\day06.exe ..\..\..\inputs\2024\06-input.txt
+@Release\day08.exe ..\..\..\inputs\2024\08-input.txt
