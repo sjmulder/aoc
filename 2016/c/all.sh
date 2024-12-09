3088e670cafd2c706752c0f78b4374dbe5e42c18
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-09 13:39:55 +0100

    Run sequentially

--- 2016/c/all.sh
+++ 2016/c/all.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo 01: $(./day01-p1 <../../inputs/2016/01-input.txt) &
-echo 02: $(./day02    <../../inputs/2016/02-input.txt) &
-echo 03: $(./day03    <../../inputs/2016/03-input.txt) &
-./day06 <../../inputs/2016/06-input.txt &
-wait
+echo 01: $(./day01-p1 <../../inputs/2016/01-input.txt)
+echo 02: $(./day02    <../../inputs/2016/02-input.txt)
+echo 03: $(./day03    <../../inputs/2016/03-input.txt)
+./day06 <../../inputs/2016/06-input.txt
