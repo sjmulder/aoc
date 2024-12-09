616ba5f6eccf306b8bc5aae05d6f43c307d9fc98
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-09 13:45:16 +0100

    Much simpler 2016 day 6

--- 2016/c/day06.c
+++ 2016/c/day06.c
@@ -1,43 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <ctype.h>
-#include <assert.h>
 
 int
 main(int argc, char **argv)
 {
-	char buf[16];
-	int max_j=0, max_c=0, counts[8][256]={}, i,j;
-	int min_j=0, min_c=0;
+	char buf[16], p1[9]="aaaaaaaa", p2[9]="aaaaaaaa";
+	int counts[8][256]={}, i,j;
 
 	if (argc > 1)
 		freopen(argv[1], "r", stdin);
 
 	while (fgets(buf, sizeof(buf), stdin))
-		for (i=0; i<8 && isalnum(buf[i]); i++)
+		for (i=0; i<8 && buf[i] >= 'a' && buf[i] <= 'z'; i++)
 			counts[i][(int)buf[i]]++;
-	
-	printf("06: ");
 
-	/* TODO: single pass for min and max into ans[] strings */
-
-	for (i=0; i<8; i++) {
-		for (j='a', max_c=0; j <= 'z'; j++)
-			if (counts[i][j] > max_c)
-				{ max_j = j; max_c = counts[i][j]; }
-		putchar(max_j);
+	for (i=0; i<8; i++)
+	for (j='a'; j<='z'; j++) {
+		if (counts[i][j] > counts[i][(int)p1[i]]) p1[i] = j;
+		if (counts[i][j] < counts[i][(int)p2[i]]) p2[i] = j;
 	}
 
-	putchar(' ');
-
-	for (i=0; i<8; i++) {
-		for (j='a', min_c=INT_MAX; j <= 'z'; j++)
-			if (counts[i][j] < min_c)
-				{ min_j = j; min_c = counts[i][j]; }
-		putchar(min_j);
-	}
-
-	putchar('\n');
+	printf("06: %s %s\n", p1, p2);
 	return 0;
 }
