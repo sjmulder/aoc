bc8219e30ab387b674993b4a73c2526e67806787
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-13 22:38:38 +0100

    2024 day 13

---
+++ 2024/c/day13.c
@@ -0,0 +1,45 @@
+#include "common.h"
+
+static int64_t
+score(int ax, int ay, int bx, int by, int64_t px, int64_t py)
+{
+	int64_t a,b, x;
+	double as,bs;
+
+	as = (double)ay / ax;
+	bs = (double)by / bx;
+
+	/* intersection between a (from start) and b (from end) */
+	x = (int64_t)round((px*bs - py) / (bs-as));
+
+	a = (x+i) / ax;
+	b = (px-x-i) / bx;
+
+	return
+	    a*ax + b*bx == px &&
+	    a*ay + b*by == py ? a*3 + b;
+}
+
+int
+main(int argc, char **argv)
+{
+	int ax,ay, bx,by;
+	int64_t p1=0,p2=0, px,py;
+
+	if (argc > 1)
+		DISCARD(freopen(argv[1], "r", stdin));
+	
+	while (scanf(
+	    " Button A: X+%d, Y+%d"
+	    " Button B: X+%d, Y+%d"
+	    " Prize: X=%"SCNd64", Y=%"SCNd64,
+	    &ax, &ay, &bx, &by, &px, &py) == 6) {
+		p1 += score(ax,ay, bx,by, px,py);
+		p2 += score(ax,ay, bx,by,
+		    px + 10000000000000LL,
+		    py + 10000000000000LL);
+	}
+
+	printf("13: %"PRId64" %"PRId64"\n", p1, p2);
+	return 0;
+}
