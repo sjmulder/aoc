ddaa04a78b7c19942ed8bae572451573fd52ccab
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-14 22:08:46 +0100

    Revert "Faster (but nasty?) heuristic"
    Looking at other people's output shows the tree-in-quadrant assumption
    to be incorrect.
    
    This reverts commit 6ee76db92231e80aa672b66712f158e123b33535.

--- 2024/c/day14.c
+++ 2024/c/day14.c
@@ -8,8 +8,10 @@
 int
 main(int argc, char **argv)
 {
+	static char g[GH][GW];
 	static int px[NR],py[NR], vx[NR],vy[NR];
-	int p1=0, n=0, sec, i, q[4]={};
+
+	int p1=0, n=0, sec, i, x,y, q[4]={}, run;
 
 	if (argc > 1)
 		DISCARD(freopen(argv[1], "r", stdin));
@@ -18,27 +20,34 @@ main(int argc, char **argv)
 		assert(n+1 < NR);
 
 	for (sec=1; !SAMPLE || sec <= 100; sec++) {
+		memset(g, 0, sizeof(g));
 		memset(q, 0, sizeof(q));
 
 		for (i=0; i<n; i++) {
 			px[i] = (px[i] + vx[i] + GW) % GW;
 			py[i] = (py[i] + vy[i] + GH) % GH;
 
-			if (px[i] < GW/2) {
-				if (py[i] < GH/2) q[0]++; else
-				if (py[i] > GH/2) q[1]++;
-			} else if (px[i] > GW/2) {
-				if (py[i] < GH/2) q[2]++; else
-				if (py[i] > GH/2) q[3]++;
+			g[py[i]][px[i]] = 1;
+
+			if (sec == 100) {
+				if (px[i] < GW/2) {
+					if (py[i] < GH/2) q[0]++; else
+					if (py[i] > GH/2) q[1]++;
+				} else if (px[i] > GW/2) {
+					if (py[i] < GH/2) q[2]++; else
+					if (py[i] > GH/2) q[3]++;
+				}
 			}
 		}
 
 		if (sec == 100)
 			p1 = q[0]*q[1]*q[2]*q[3];
 
-		/* robots cosplaying a tree in the corner? */
-		for (i=0; i<4; i++)
-			if (q[i] > n/2)
+		for (y=0; y<GH; y++)
+		for (x=0, run=0; x<GW; x++)
+			if (!g[y][x])
+				run = 0;
+			else if (++run >= 10)
 				goto found_p2;
 	}
 
