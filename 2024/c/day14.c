80136d88420bcda209ffb16b16ed2c58ceee88b4
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-14 21:18:23 +0100

    Simpler heuristic

--- 2024/c/day14.c
+++ 2024/c/day14.c
@@ -11,51 +11,38 @@ main(int argc, char **argv)
 	static char g[GH][GW];
 	static int px[NR],py[NR], vx[NR],vy[NR];
 
-	int nr, nt, i,j, x,y;
-	uint64_t order, max_order=0, acc;
+	int nr, ntok, sec,i, x,y, run;
 
 	if (argc > 1)
 		DISCARD(freopen(argv[1], "r", stdin));
 
 	for (nr=0; nr<NR; nr++) {
-		nt = scanf(" p=%d,%d v=%d,%d",
+		ntok = scanf(" p=%d,%d v=%d,%d",
 		    &px[nr], &py[nr],
 		    &vx[nr], &vy[nr]);
-		if (nt != 4)
+		if (ntok != 4)
 			break;
 	}
 
-	for (i=0; i < 5*60*60; i++) {
+	for (sec=1; ; sec++) {
 		memset(g, 0, sizeof(g));
 
-		for (j=0; j<nr; j++) {
-			px[j] = (px[j] + vx[j] + GW) % GW;
-			py[j] = (py[j] + vy[j] + GH) % GH;
-			g[py[j]][px[j]] = 1;
+		for (i=0; i<nr; i++) {
+			px[i] = (px[i] + vx[i] + GW) % GW;
+			py[i] = (py[i] + vy[i] + GH) % GH;
+
+			g[py[i]][px[i]] = 1;
 		}
 
-		order = 0;
-
-		for (y=0; y<GH; y++) {
-			for (acc=1, x=0; x<GW; x++)
-				if (g[y][x])
-					acc *= 2;
-			order += acc;
-		}
-
-		for (x=0; x<GW; x++) {
-			for (acc=1, y=0; y<GH; y++)
-				if (g[y][x])
-					acc *= 2;
-			order += acc;
-		}
-
-		if (order >= max_order) {
-			printf("%d (%d:%02d:%02d): order %"PRIu64"\n",
-			    i, i/60/60, i/60%60, i%60, order);
-			max_order = order;
-		}
+		for (y=0; y<GH; y++)
+		for (x=0, run=0; x<GW; x++)
+			if (!g[y][x])
+				run = 0;
+			else if (++run >= 10)
+				goto found_p2;
 	}
 
+found_p2:
+	printf("14: __ %d\n", sec);
 	return 0;
 }
