1200c77ba6fb8a4355e2cd4a33039e300e890ac7
Sijmen J. Mulder <ik@sjmulder.nl>
2024-12-19 13:06:01 +0100

    Work incrementally by _removing_ points
    From a helpful comment by @Acters@lemmy.world:
    
    > Part 2 can be faster if you iteratively remove blocks until there is a
    > path. This is because it is faster to fail to find a path and the
    > flood fill algorithm does not need to fill as many spots because the
    > map would be filled up with more blocks! this drops the part 2 solve
    > to a few milliseconds. others have taken a binary search option which
    > is faster.
    
    Still not very fast, bad flood algo.

--- 2024/c/day18.c
+++ 2024/c/day18.c
@@ -1,8 +1,9 @@
 #include "common.h"
 
 #define SAMPLE	0
+#define PTZ	3600
 #define GZ	(SAMPLE ? 9 : 73)
-#define NCORR	(SAMPLE ? 12 : 1024)
+#define P1STEP	(SAMPLE ? 12 : 1024)
 #define CORR	-1
 
 static int g[GZ][GZ];
@@ -51,7 +52,9 @@ flood(void)
 int
 main(int argc, char **argv)
 {
-	int p1=0, x,y, i;
+	static int xs[PTZ], ys[PTZ];
+	static char p2[32];
+	int p1=0, npt=0, i;
 
 	if (argc > 1)
 		DISCARD(freopen(argv[1], "r", stdin));
@@ -59,27 +62,34 @@ main(int argc, char **argv)
 	for (i=0; i<GZ; i++)
 		g[0][i] = g[GZ-1][i] =
 		g[i][0] = g[i][GZ-1] = CORR;
-	
+
+	for (npt=0; npt<PTZ && scanf(" %d,%d", xs+npt, ys+npt)==2; npt++) {
+		assert(xs[npt] >= 0); assert(xs[npt] < GZ-2);
+		assert(ys[npt] >= 0); assert(ys[npt] < GZ-2);
+	}
+
+	assert(npt < PTZ);
+
+	for (i=0; i<npt; i++)
+		g[ys[i]+1][xs[i]+1] = CORR;
+
 	g[1][1] = 1;
+	flood();
 
-	for (i=0; scanf(" %d,%d", &x, &y) == 2; i++) {
-		assert(x >= 0); assert(x < GZ-2);
-		assert(y >= 0); assert(y < GZ-2);
-		g[y+1][x+1] = CORR;
+	for (i=npt-1; i >= P1STEP; i--) {
+		g[ys[i]+1][xs[i]+1] = 0;
 
-		if (i < NCORR-1)
+		if (!flood_step(xs[i]+1, ys[i]+1))
 			continue;
 
 		flood();
 
-		if (i==NCORR-1)
-			p1 = g[GZ-2][GZ-2]-1;
-		if (g[GZ-2][GZ-2] <= 0) {
-			printf("18: %d %d,%d\n", p1, x,y);
-			return 0;
-		}
+		if (!p2[0] && g[GZ-2][GZ-2] > 0)
+			snprintf(p2, sizeof(p2), "%d,%d", xs[i],ys[i]);
 	}
 
-	assert(!"no solution");
-	return -1;
+	p1 = g[GZ-2][GZ-2]-1;
+
+	printf("18: %d %s\n", p1, p2);
+	return 0;
 }
